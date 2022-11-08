311e1bda7e72c1116201b082a7673d7985e197a7
Filip Ślęzak <102536422+filipslezaklab@users.noreply.github.com>
2022-11-08 13:06:37 +0100

    Fix lag on modal opening animation (#25)
    * Fix lag on modal opening animation

--- web/src/shared/components/layout/QrCode/QrCode.tsx
+++
@@ -1,8 +0,0 @@
-import { QRCode } from 'react-qrcode';
-
-interface Props {
-  data: string;
-}
-export const QrCode = ({ data }: Props) => {
-  return <QRCode value={data} className="qr-code" width={300} />;
-};
