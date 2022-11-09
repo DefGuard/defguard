eb2cb4d561986fb312756aa8e6e01da386e2f6ca
dzania <artur.kantorczyk@gmail.com>
2022-11-09 23:17:50 +0100

    fix: store name remove unused mixins

--- web/src/pages/overview/hooks/store/useOverviewStore.ts
+++ web/src/pages/overview/hooks/store/useOverviewStore.ts
@@ -3,14 +3,22 @@ import { persist } from 'zustand/middleware';
 
 import { OverviewLayoutType, OverviewStore } from '../../../../shared/types';
 
-export const useOverviewStore = create<OverviewStore, [
+export const useOverviewStore = create<
+  OverviewStore,
+  [
     [
       'zustand/persist',
       Pick<OverviewStore, 'viewMode' | 'defaultViewMode' | 'statsFilter'>
     ]
-]>(persist((set) => ({
-  viewMode: OverviewLayoutType.GRID,
-	defaultViewMode: OverviewLayoutType.GRID,
-  statsFilter: 1,
-  setState: (newValues) => set((state) => ({ ...state, ...newValues })),
-})));
+  ]
+>(
+  persist(
+    (set) => ({
+      viewMode: OverviewLayoutType.GRID,
+      defaultViewMode: OverviewLayoutType.GRID,
+      statsFilter: 1,
+      setState: (newValues) => set((state) => ({ ...state, ...newValues })),
+    }),
+    { name: 'overview-store', getStorage: () => sessionStorage }
+  )
+);
