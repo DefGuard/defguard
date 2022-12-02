70ff576930eb3c0d8e02dc22df5f6341bd742733
Filip Ślęzak <102536422+filipslezaklab@users.noreply.github.com>
2022-12-02 09:40:52 +0100

    check gateway status frontend (#60)

--- web/src/pages/network/NetworkGateway/NetworkGateway.tsx
+++ web/src/pages/network/NetworkGateway/NetworkGateway.tsx
@@ -1,5 +1,7 @@
 import './style.scss';
 
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+
 import Button, {
   ButtonSize,
   ButtonStyleVariant,
@@ -8,8 +10,27 @@ import { Card } from '../../../shared/components/layout/Card/Card';
 import MessageBox, {
   MessageBoxType,
 } from '../../../shared/components/layout/MessageBox/MessageBox';
+import useApi from '../../../shared/hooks/useApi';
+import { useToaster } from '../../../shared/hooks/useToaster';
+import { QueryKeys } from '../../../shared/queries';
 
 export const NetworkGatewaySetup = () => {
+  const toaster = useToaster();
+  const {
+    network: { getGatewayStatus },
+  } = useApi();
+  const queryClient = useQueryClient();
+  const { data: gatewayStatus, isLoading: statusLoading } = useQuery(
+    [QueryKeys.FETCH_GATEWAY_STATUS],
+    getGatewayStatus,
+    {
+      onError: (err) => {
+        toaster.error('Failed to get gateway status');
+        console.error(err);
+      },
+      refetchOnWindowFocus: false,
+    }
+  );
   return (
     <section className="gateway">
       <header>
@@ -28,15 +49,23 @@ export const NetworkGatewaySetup = () => {
             size={ButtonSize.BIG}
             styleVariant={ButtonStyleVariant.PRIMARY}
             text="Check connection status"
+            loading={statusLoading}
+            onClick={() => {
+              if (!statusLoading) {
+                queryClient.invalidateQueries([QueryKeys.FETCH_GATEWAY_STATUS]);
+              }
+            }}
           />
-          <MessageBox type={MessageBoxType.ERROR}>
-            <p>No connection established, please run provided command.</p>
-          </MessageBox>
-          {/**
-          <MessageBox type={MessageBoxType.SUCCESS}>
-            <p>Gateway connected.</p>
-          </MessageBox>
-            **/}
+          {!gatewayStatus && !statusLoading && (
+            <MessageBox type={MessageBoxType.ERROR}>
+              <p>No connection established, please run provided command.</p>
+            </MessageBox>
+          )}
+          {gatewayStatus && !statusLoading && (
+            <MessageBox type={MessageBoxType.SUCCESS}>
+              <p>Gateway connected.</p>
+            </MessageBox>
+          )}
         </div>
       </Card>
     </section>
