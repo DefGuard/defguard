25a74df03b458fd625d75c6aaf161fd03c1c9d59
dzania <artur.kantorczyk@gmail.com>
2022-12-07 09:27:05 +0100

    fix: migrations and remove INNER

--- migrations/20221205152009_extend_oauth2token.up.sql
+++ migrations/20221205152009_extend_oauth2token.up.sql
@@ -1,2 +1,3 @@
-ALTER TABLE oauth2token ADD client_id bigint NOT NULL;
+DELETE FROM oauth2token;
+ALTER TABLE oauth2token ADD COLUMN client_id bigint NOT NULL;
 ALTER TABLE oauth2token ADD FOREIGN KEY(client_id) REFERENCES "oauth2client"(id) ON DELETE CASCADE;
