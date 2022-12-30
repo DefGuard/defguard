4b759cdd3d36cb1dbac2ec01b52a1da0264e375a
dzania <artur.kantorczyk@gmail.com>
2022-12-30 11:26:11 +0100

    add default path in migrations

--- migrations/20221228104814_extend_settings.up.sql
+++ migrations/20221228104814_extend_settings.up.sql
@@ -1,2 +1,2 @@
 ALTER TABLE settings ADD instance_name text NOT NULL DEFAULT 'Defguard';
-ALTER TABLE settings ADD logo_url text NOT NULL DEFAULT '/src/shared/images/svg/defguad-nav-logo.svg';
+ALTER TABLE settings ADD logo_url text NOT NULL DEFAULT '/defguad-nav-logo.svg';
