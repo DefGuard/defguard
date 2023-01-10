d44812c204d1349f810314467256bb6616d99f10
dzania <artur.kantorczyk@gmail.com>
2023-01-10 11:34:42 +0100

    Remove session import

--- src/handlers/license.rs
+++ src/handlers/license.rs
@@ -1,4 +1,4 @@
-use crate::{appstate::AppState, auth::SessionInfo, handlers::ApiResponse, license::License};
+use crate::{appstate::AppState, handlers::ApiResponse, license::License};
 use rocket::{http::Status, serde::json::serde_json::json, State};
 
 #[get("/license", format = "json")]
