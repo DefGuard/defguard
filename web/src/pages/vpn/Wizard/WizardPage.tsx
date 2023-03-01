c49bd55c44641b36d13b5c5f4459693dc951c4ac
Jacek Chmielewski <jchmielewski@teonite.com>
2023-03-01 07:56:15 +0100

    Embed wizard in PageContainer (navigation)

--- web/src/pages/vpn/Wizard/WizardPage.tsx
+++ web/src/pages/vpn/Wizard/WizardPage.tsx
@@ -9,34 +9,37 @@ import { MobileBanner } from './MobileBanner/MobileBanner';
 import Welcome from './Welcome/Welcome';
 import WizardLogo from './WizardLogo/WizardLogo';
 import Steps from './WizardSteps/WizardSteps';
+import PageContainer from '../../../shared/components/layout/PageContainer/PageContainer';
 
 const WizardPage: React.FC = () => {
   const { breakpoint } = useBreakpoint(deviceBreakpoints);
   const windowHref = window.location.href.split('/');
   return (
-    <section
-      id="wizard"
-      className={
-        windowHref[windowHref.length - 2] +
-          windowHref[windowHref.length - 1] ===
-          'wizard' || windowHref[windowHref.length - 1] === 'wizard'
-          ? 'center'
-          : ''
-      }
-    >
-      <div className="content">
-        {breakpoint === 'desktop' && <WizardLogo />}
-        {breakpoint !== 'desktop' && <MobileBanner />}
+    <PageContainer id="wizard-page">
+      <section
+        id="wizard"
+        className={
+          windowHref[windowHref.length - 2] +
+            windowHref[windowHref.length - 1] ===
+            'wizard' || windowHref[windowHref.length - 1] === 'wizard'
+            ? 'center'
+            : ''
+        }
+      >
+        <div className="content">
+          {breakpoint === 'desktop' && <WizardLogo />}
+          {breakpoint !== 'desktop' && <MobileBanner />}
 
-        <div className="steps-container">
-          <Routes>
-            <Route index element={<Welcome />} />
-            <Route path="/:step" element={<Steps />} />
-            <Route path="*" element={<Navigate to="" />} />
-          </Routes>
+          <div className="steps-container">
+            <Routes>
+              <Route index element={<Welcome />} />
+              <Route path="/:step" element={<Steps />} />
+              <Route path="*" element={<Navigate to="" />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </PageContainer>
   );
 };
 
