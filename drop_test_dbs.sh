8ff35734ce66fb99f1cbf18bf87144b84850b016
Maciek <mwojcik@teonite.com>
2023-04-19 10:40:48 +0200

    fix(API): prevent authorization_code reuse (#146)
    * add debug log
    ---------
    
    Co-authored-by: Maciej Wójcik <wojcik91@gmail.com>

---
+++ drop_test_dbs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Removes all test databases
+# Test databases are named with UUID strings
+
+set -eo pipefail
+
+if [ -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+if [ -z "${DATABASE_URL}" ]; then
+    echo "DATABASE_URL is not set"
+    exit 1
+fi
+
+PATTERN="[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+
+echo "Dropping test databases"
+
+psql "${DATABASE_URL}" -c "copy (select datname from pg_database where datname ~ '$PATTERN') to stdout" | while read dbname; do
+    echo "Dropping $dbname"
+    psql "${DATABASE_URL}" -c "DROP DATABASE \"$dbname\""
+done
+echo
+echo "Test databases were deleted!"
+echo
+exit
