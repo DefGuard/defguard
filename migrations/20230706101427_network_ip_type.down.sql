e6cf5a596c031026f0dac5194e6e67e653cb0746
Maciej Wójcik <wojcik91@gmail.com>
2023-07-10 12:44:56 +0200

    fix stats index

--- migrations/20230706101427_network_ip_type.down.sql
+++ migrations/20230706101427_network_ip_type.down.sql
@@ -2,3 +2,6 @@ ALTER TABLE wireguard_network_device ALTER COLUMN wireguard_ip type text;
 
 ALTER TABLE wireguard_network_device ALTER COLUMN device_id DROP NOT NULL;
 ALTER TABLE wireguard_network_device ALTER COLUMN wireguard_network_id DROP NOT NULL;
+
+DROP INDEX peer_stats_device_id_collected_at;
+CREATE INDEX peer_stats_device_id_collected_at on wireguard_peer_stats (device_id, collected_at);
--- migrations/20230706101427_network_ip_type.up.sql
+++ migrations/20230706101427_network_ip_type.up.sql
@@ -2,3 +2,6 @@ ALTER TABLE wireguard_network_device ALTER COLUMN wireguard_ip type inet USING w
 
 ALTER TABLE wireguard_network_device ALTER COLUMN device_id SET NOT NULL;
 ALTER TABLE wireguard_network_device ALTER COLUMN wireguard_network_id SET NOT NULL;
+
+DROP INDEX peer_stats_device_id_collected_at;
+CREATE INDEX peer_stats_device_id_collected_at on wireguard_peer_stats (device_id, network, collected_at DESC);
