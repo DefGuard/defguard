d475bcaaf74f0c2ee5f7c343a5dddd0cbb1cfaa0
Maciej Wójcik <wojcik91@gmail.com>
2023-07-10 12:54:04 +0200

    add another column to index

--- migrations/20230706101427_network_ip_type.up.sql
+++ migrations/20230706101427_network_ip_type.up.sql
@@ -4,4 +4,4 @@ ALTER TABLE wireguard_network_device ALTER COLUMN device_id SET NOT NULL;
 ALTER TABLE wireguard_network_device ALTER COLUMN wireguard_network_id SET NOT NULL;
 
 DROP INDEX peer_stats_device_id_collected_at;
-CREATE INDEX peer_stats_device_id_collected_at on wireguard_peer_stats (device_id, network, collected_at DESC);
+CREATE INDEX peer_stats_device_id_collected_at on wireguard_peer_stats (device_id, network, collected_at DESC, latest_handshake DESC NULLS LAST);
