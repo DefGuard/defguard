5428121331e01d7c26eb8ed2dd107b1adab54d3c
Maciej Wójcik <wojcik91@gmail.com>
2023-07-25 15:00:05 +0200

    remove trigger column

--- migrations/20230725115257_wireguard_stats_purge.down.sql
+++ migrations/20230725115257_wireguard_stats_purge.down.sql
@@ -1,3 +1,1 @@
 DROP TABLE wireguard_stats_purge;
-
-DROP TYPE stats_purge_trigger;
--- migrations/20230725115257_wireguard_stats_purge.up.sql
+++ migrations/20230725115257_wireguard_stats_purge.up.sql
@@ -1,10 +1,7 @@
-CREATE TYPE stats_purge_trigger AS ENUM ('periodic_task', 'cli');
-
 CREATE TABLE wireguard_stats_purge (
     id bigserial PRIMARY KEY,
     started_at timestamp without time zone NOT NULL,
     finished_at timestamp without time zone NOT NULL,
     removal_threshold timestamp without time zone NOT NULL,
-    records_removed bigint NOT NULL,
-    triggered_by stats_purge_trigger
+    records_removed bigint NOT NULL
 );
