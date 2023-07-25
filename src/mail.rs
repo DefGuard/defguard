1266f64d42184807ed271db8560865f5c85c0de5
Jacek Chmielewski <jchmielewski@teonite.com>
2023-07-25 14:43:18 +0200

    Initial smtp handling with lettre library

--- src/mail.rs
+++ src/mail.rs
@@ -1,4 +1,20 @@
+use lettre::{
+    address::AddressError,
+    message::{header::ContentType, Mailbox},
+    transport::smtp::authentication::Credentials,
+    Address, AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor,
+};
+use thiserror::Error;
 use tokio::sync::mpsc::UnboundedReceiver;
+
+#[derive(Error, Debug)]
+pub enum MailError {
+    #[error(transparent)]
+    LettreError(#[from] lettre::error::Error),
+
+    #[error(transparent)]
+    AddressError(#[from] lettre::address::AddressError),
+}
 
 #[derive(Debug, Clone)]
 pub struct Mail {
@@ -6,6 +22,29 @@ pub struct Mail {
     pub to: String,
     pub subject: String,
     pub content: String,
+}
+
+/// Builds Mailbox structure from string representing user email address
+fn mailbox(address: &str) -> Result<Mailbox, MailError> {
+    let mut split = address.split('@');
+    let (user, domain) = (
+        split.next().ok_or(AddressError::MissingParts)?,
+        split.next().ok_or(AddressError::MissingParts)?,
+    );
+    Ok(Mailbox::new(None, Address::new(user, domain)?))
+}
+
+impl TryFrom<Mail> for Message {
+    type Error = MailError;
+
+    fn try_from(mail: Mail) -> Result<Self, Self::Error> {
+        Ok(Self::builder()
+            .from(mailbox(&mail.from)?)
+            .to(mailbox(&mail.to)?)
+            .subject(mail.subject)
+            .header(ContentType::TEXT_HTML)
+            .body(mail.content)?)
+    }
 }
 
 pub struct MailHandler {
@@ -20,8 +59,29 @@ impl MailHandler {
     pub async fn run(mut self) {
         while let Some(mail) = self.rx.recv().await {
             debug!("Sending mail: {mail:?}");
-            // TODO
-            info!("Sent mail: {mail:?}");
+
+            let message: Message = match mail.clone().try_into() {
+                Ok(message) => message,
+                Err(err) => {
+                    error!("Failed to build message: {mail:?}, {err}");
+                    continue;
+                }
+            };
+
+            // TODO: smtp config
+            let credentials = Credentials::new(String::new(), String::new());
+            // TODO: consider creating in constructor
+            let mailer = match AsyncSmtpTransport::<Tokio1Executor>::relay("") {
+                Ok(mailer) => mailer.credentials(credentials).build(),
+                Err(err) => {
+                    error!("Failed to create mailer: {err}");
+                    continue;
+                }
+            };
+            match mailer.send(message).await {
+                Ok(response) => info!("Mail sent successfully: {mail:?}, {response:?}"),
+                Err(err) => error!("Email sending failed: {mail:?}, {err}"),
+            }
         }
     }
 }
