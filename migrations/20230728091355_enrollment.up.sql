90c823865ba15cfdeab618fe41070bf1985c587a
Maciej Wójcik <wojcik91@gmail.com>
2023-07-28 11:21:26 +0200

    add enrollment table

---
+++ migrations/20230728091355_enrollment.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE enrollment (
+    id text PRIMARY KEY NOT NULL,
+    user_id bigint NOT NULL,
+    admin_id bigint NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    expires_at timestamp without time zone NOT NULL,
+    used_at timestamp without time zone,
+    FOREIGN KEY(user_id) REFERENCES "user"(id),
+    FOREIGN KEY(admin_id) REFERENCES "user"(id)
+);
