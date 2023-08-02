0948c725c3995ac7e54c49d4b3c8d8f66ebce0e8
Maciej Wójcik <wojcik91@gmail.com>
2023-08-02 13:25:43 +0200

    add enrollment settings columns

--- migrations/20230728091355_enrollment.up.sql
+++ migrations/20230728091355_enrollment.up.sql
@@ -11,6 +11,7 @@ CREATE TABLE enrollment (
 
 ALTER TABLE "user" ALTER COLUMN password_hash DROP NOT NULL;
 
-ALTER TABLE settings ADD COLUMN enrollment_vpn_step_optional boolean NULL;
+ALTER TABLE settings ADD COLUMN enrollment_vpn_step_optional boolean NOT NULL default true;
 ALTER TABLE settings ADD COLUMN enrollment_welcome_message text NULL;
 ALTER TABLE settings ADD COLUMN enrollment_welcome_email text NULL;
+ALTER TABLE settings ADD COLUMN enrollment_use_welcome_message_as_email boolean NOT NULL default true;
