1b736fce2bc3fbad449742252273eaaf307fe766
Maciej Wójcik <wojcik91@gmail.com>
2023-08-09 12:57:37 +0200

    lint fix

--- web/src/pages/settings/EnrollmentTab/EnrollmentTab.tsx
+++ web/src/pages/settings/EnrollmentTab/EnrollmentTab.tsx
@@ -10,6 +10,7 @@ import * as yup from 'yup';
 
 import { useI18nContext } from '../../../i18n/i18n-react';
 import { FormCheckBox } from '../../../shared/components/Form/FormCheckBox/FormCheckBox';
+import { FormInput } from '../../../shared/components/Form/FormInput/FormInput';
 import { FormSelect } from '../../../shared/components/Form/FormSelect/FormSelect';
 import { Button } from '../../../shared/components/layout/Button/Button';
 import {
@@ -33,7 +34,6 @@ import { useToaster } from '../../../shared/hooks/useToaster';
 import { MutationKeys } from '../../../shared/mutations';
 import { QueryKeys } from '../../../shared/queries';
 import { Settings } from '../../../shared/types';
-import {FormInput} from "../../../shared/components/Form/FormInput/FormInput";
 
 interface Inputs extends Omit<Settings, 'enrollment_vpn_step_optional'> {
   enrollment_vpn_step_optional: SelectOption<boolean>;
