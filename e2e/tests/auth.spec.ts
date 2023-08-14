aae55c3158be3dffc094d5171df2d55501ee234d
Kamil Chudy <kchudy@teonite.com>
2023-08-14 11:50:29 +0200

    284 e2e test change user password (#302)
    Added tests for changing user password

--- e2e/tests/auth.spec.ts
+++ e2e/tests/auth.spec.ts
@@ -5,7 +5,9 @@ import { defaultUserAdmin, routes } from '../config';
 import { acceptRecovery } from '../utils/controllers/acceptRecovery';
 import { createUser } from '../utils/controllers/createUser';
 import { loginBasic, loginRecoveryCodes, loginTOTP } from '../utils/controllers/login';
+import { logout } from '../utils/controllers/logout';
 import { enableTOTP } from '../utils/controllers/mfa/enableTOTP';
+import { changePassword, changePasswordByAdmin } from '../utils/controllers/profile';
 import { dockerRestart } from '../utils/docker';
 import { waitForBase } from '../utils/waitForBase';
 import { waitForRoute } from '../utils/waitForRoute';
@@ -73,4 +75,28 @@ test('Add user to admin group', async ({ page, context }) => {
   await loginBasic(page, testUser);
   await waitForRoute(page, routes.admin.wizard);
   expect(page.url()).toBe(routes.base + routes.admin.wizard);
+});
+
+test('Change user password', async ({ page, context }) => {
+  await waitForBase(page);
+  const testUser = await createUser(context, faker.person.firstName().toLowerCase());
+  await loginBasic(page, testUser);
+  testUser.password = await changePassword(page, testUser.password);
+  await logout(page);
+  await loginBasic(page, testUser);
+  await waitForRoute(page, routes.me);
+  expect(page.url()).toBe(routes.base + routes.me);
+});
+
+test('Change user password by admin', async ({ page, context }) => {
+  await waitForBase(page);
+  const testUser = await createUser(context, faker.person.firstName().toLowerCase());
+  await loginBasic(page, defaultUserAdmin);
+  await page.goto(routes.base + routes.admin.users);
+  await page.getByText(testUser.username).click();
+  testUser.password = await changePasswordByAdmin(page);
+  await logout(page);
+  await loginBasic(page, testUser);
+  await waitForRoute(page, routes.me);
+  expect(page.url()).toBe(routes.base + routes.me);
 });
---
+++ e2e/utils/controllers/profile.ts
@@ -0,0 +1,31 @@
+import { Page } from 'playwright';
+
+import { routes } from '../../config';
+
+export const changePassword = async (page: Page, currentPassword: string) => {
+  await page.goto(routes.base + routes.me);
+  await page.getByTestId('edit-user').click();
+  await page.getByTestId('button-change-password').click();
+  const formElement = page.getByTestId('change-self-password-form');
+  await formElement.waitFor({ state: 'visible' });
+  await formElement.getByTestId('field-old_password').type(currentPassword);
+  const newPassword = 'Test1234#$%';
+  await formElement.getByTestId('field-new_password').type(newPassword);
+  await formElement.getByTestId('field-repeat').type(newPassword);
+  await formElement.locator('button[type="submit"]').click();
+  await formElement.waitFor({ state: 'hidden', timeout: 2000 });
+  return newPassword;
+};
+
+export const changePasswordByAdmin = async (page: Page) => {
+  await page.getByTestId('edit-user').click();
+  await page.getByTestId('button-change-password').click();
+  const formElement = page.getByTestId('change-password-admin-form');
+  await formElement.waitFor({ state: 'visible' });
+  const newPassword = 'Test1234#$%';
+  await formElement.getByTestId('field-new_password').type(newPassword);
+  await formElement.getByTestId('field-repeat').type(newPassword);
+  await formElement.locator('button[type="submit"]').click();
+  await formElement.waitFor({ state: 'hidden', timeout: 2000 });
+  return newPassword;
+};
