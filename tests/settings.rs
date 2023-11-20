fbc1541133c5895c305c799bccc7ed63d9f5a19d
Filip Slezak <fslezak@teonite.com>
2023-11-20 12:09:59 +0100

    fix integration tests for settings

--- tests/settings.rs
+++ tests/settings.rs
@@ -2,7 +2,10 @@ mod common;
 
 use axum::http::StatusCode;
 use common::ClientState;
-use defguard::{db::models::settings::Settings, handlers::Auth};
+use defguard::{
+    db::models::settings::{Settings, SettingsPatch},
+    handlers::Auth,
+};
 
 use self::common::{client::TestClient, make_test_client};
 
@@ -13,28 +16,38 @@ async fn make_client() -> (TestClient, ClientState) {
 
 #[tokio::test]
 async fn test_settings() {
-    let (client, client_state) = make_client().await;
-
+    let (client, _client_state) = make_client().await;
     let auth = Auth::new("admin".into(), "pass123".into());
     let response = &client.post("/api/v1/auth").json(&auth).send().await;
     assert_eq!(response.status(), StatusCode::OK);
-    Settings::init_defaults(&client_state.pool).await.unwrap();
-    let db_settings = Settings::get_settings(&client_state.pool).await.unwrap();
-
     // get settings
     let response = client.get("/api/v1/settings").send().await;
     assert_eq!(response.status(), StatusCode::OK);
     let mut settings: Settings = response.json().await;
-    assert_eq!(db_settings, settings);
     // modify settings
     settings.wireguard_enabled = false;
     settings.challenge_template = "Modified".to_string();
     let response = client.put("/api/v1/settings").json(&settings).send().await;
     assert_eq!(response.status(), StatusCode::OK);
-
     // verify modified settings
     let response = client.get("/api/v1/settings").send().await;
     assert_eq!(response.status(), StatusCode::OK);
     let new_settings: Settings = response.json().await;
     assert_eq!(new_settings, settings);
+    // patch settings
+    let patch_json: &str = r#"
+    {
+        "wireguard_enabled": true
+    }"#;
+    let settings_patch: SettingsPatch = serde_json::from_str(patch_json).unwrap();
+    let response = client
+        .patch("/api/v1/settings")
+        .json(&settings_patch)
+        .send()
+        .await;
+    assert_eq!(response.status(), StatusCode::OK);
+    let response = client.get("/api/v1/settings").send().await;
+    assert_eq!(response.status(), StatusCode::OK);
+    let new_settings: Settings = response.json().await;
+    assert!(new_settings.wireguard_enabled);
 }
