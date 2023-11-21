ff9be99da91dd45a036ca329396d34b1ee502b05
Maciej Wójcik <wojcik91@gmail.com>
2023-11-21 11:58:51 +0100

    make both filters optional

--- src/handlers/ssh_authorized_keys.rs
+++ src/handlers/ssh_authorized_keys.rs
@@ -15,9 +15,9 @@ fn trim_newline(s: &mut String) {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct SshKeysRequestParams {
-    username: String,
+    username: Option<String>,
     group: Option<String>,
 }
 
@@ -25,45 +25,73 @@ pub struct SshKeysRequestParams {
 ///
 /// Meant to be used with `AuthorizedKeysCommand` config option in `sshd`.
 /// Should always return a response to partially mitigate user enumeration.
-/// Requires `username` query param and optionally `group` for further filtering
-/// (for example to only authorize admin users).
+/// Optional query params `username` and `group` are used for filtering users.
+/// If no params are specified an empty response is returned.
 pub async fn get_authorized_keys(
     params: Query<SshKeysRequestParams>,
     State(appstate): State<AppState>,
 ) -> Result<String, WebError> {
-    info!("Fetching public SSH keys for user {}", params.username);
+    info!("Fetching public SSH keys for {:?}", params);
     let mut ssh_keys = Vec::new();
 
-    // find user by username
-    if let Some(user) = User::find_by_username(&appstate.pool, &params.username).await? {
-        // check if user belongs to specified group
-        if let Some(group_name) = &params.group {
-            match Group::find_by_name(&appstate.pool, group_name).await? {
-                Some(group) => {
-                    let members = group.member_usernames(&appstate.pool).await?;
-                    if !members.contains(&user.username) {
-                        debug!(
-                            "User {} is not a member of group {group_name}",
-                            user.username
-                        );
-                        return Ok("".into());
-                    }
-                }
-                None => {
-                    error!("Group {group_name} not found");
-                    return Ok("".into());
-                }
-            }
-        }
-
+    let mut add_user_keys_to_list = |user: User| {
         // add key to list if user has an assigned SSH key
         if let Some(mut key) = user.ssh_key {
             trim_newline(&mut key);
             ssh_keys.push(key);
         }
-    } else {
-        debug!("Specified user does not exist")
+    };
+
+    // check if group filter was specified
+    match &params.group {
+        Some(group_name) => {
+            // fetch group
+            if let Some(group) = Group::find_by_name(&appstate.pool, group_name).await? {
+                // check if user filter was specified
+                match &params.username {
+                    Some(username) => {
+                        debug!("Fetching SSH keys for user {username} in group {group_name}");
+                        // fetch user
+                        if let Some(user) = User::find_by_username(&appstate.pool, username).await?
+                        {
+                            // check if user belongs to specified group
+                            let members = group.member_usernames(&appstate.pool).await?;
+                            if members.contains(&user.username) {
+                                add_user_keys_to_list(user);
+                            } else {
+                                debug!("User {username} is not a member of group {group_name}",);
+                            }
+                        } else {
+                            debug!("Specified user does not exist")
+                        }
+                    }
+                    None => {
+                        debug!("Fetching SSH keys for all users in group {group_name}");
+                        // fetch all users in group
+                        let users = group.fetch_all_members(&appstate.pool).await?;
+                        for user in users {
+                            add_user_keys_to_list(user)
+                        }
+                    }
+                }
+            } else {
+                debug!("Specified group does not exist")
+            }
+        }
+        None => {
+            // check if user filter was specified
+            if let Some(username) = &params.username {
+                debug!("Fetching SSH keys for user {username}");
+                // fetch user
+                if let Some(user) = User::find_by_username(&appstate.pool, username).await? {
+                    add_user_keys_to_list(user);
+                } else {
+                    debug!("Specified user does not exist")
+                }
+            }
+        }
     }
 
+    // concatenate all keys into a response
     Ok(ssh_keys.join("\n"))
 }
