06e707e6b54cd80f38c6d336f08117dbff9e399c
Filip Ślęzak <102536422+filipslezaklab@users.noreply.github.com>
2023-11-22 15:08:32 +0100

    fix: remove redirect url validation in oidc form (#426)

--- web/src/pages/openid/modals/OpenIdClientModal/OpenIdClientModalForm.tsx
+++ web/src/pages/openid/modals/OpenIdClientModal/OpenIdClientModalForm.tsx
@@ -21,7 +21,6 @@ import useApi from '../../../../shared/hooks/useApi';
 import { useClipboard } from '../../../../shared/hooks/useClipboard';
 import { useToaster } from '../../../../shared/hooks/useToaster';
 import { MutationKeys } from '../../../../shared/mutations';
-import { patternValidUrl } from '../../../../shared/patterns';
 import { QueryKeys } from '../../../../shared/queries';
 import { OpenIdClientModalFormScopes } from './components/OpenIdClientModalFormScopes';
 import { OpenIdClientFormFields, OpenIdClientScope } from './types';
@@ -106,17 +105,7 @@ export const OpenIdClientModalForm = () => {
               .string()
               .required(
                 LL.openidOverview.modals.openidClientModal.form.error.urlRequired(),
-              )
-              .test('valid-url', (val, ctx) => {
-                if (val.includes('localhost')) return true;
-                if (!patternValidUrl.test(val)) {
-                  return ctx.createError({
-                    message:
-                      LL.openidOverview.modals.openidClientModal.form.error.validUrl(),
-                  });
-                }
-                return true;
-              }),
+              ),
           })
           .required(),
       ),
