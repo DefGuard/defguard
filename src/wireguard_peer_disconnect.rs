bc8e61e0c7e637b833132db8ea28de0f48e5d45d
Maciej Wójcik <wojcik91@gmail.com>
2023-12-27 08:41:46 +0100

    finish disconnect process

--- src/wireguard_peer_disconnect.rs
+++ src/wireguard_peer_disconnect.rs
@@ -4,17 +4,34 @@
 //! it should be removed from gateway configuration and marked as "not allowed",
 //! which enforces an authentication requirement to connect again.
 
-use crate::db::{DbPool, Device, GatewayEvent, WireguardNetwork, WireguardPeerStats};
-use chrono::{DateTime, Duration as ChronoDuration, NaiveDateTime, Utc};
-use humantime::format_duration;
-use sqlx::{query, query_as, query_scalar, Error as SqlxError, PgExecutor};
+use crate::db::models::device::DeviceNetworkInfo;
+use crate::db::{
+    models::{
+        device::{DeviceInfo, WireguardNetworkDevice},
+        error::ModelError,
+        wireguard::WireguardNetworkError,
+    },
+    DbPool, Device, GatewayEvent, WireguardNetwork,
+};
+use sqlx::{query_as, Error as SqlxError};
 use std::time::Duration;
-use tokio::sync::broadcast::Sender;
-use tokio::time::sleep;
-use crate::db::models::device::DeviceInfo;
+use thiserror::Error;
+use tokio::{sync::broadcast::Sender, time::sleep};
 
 // How long to sleep between loop iterations
 const DISCONNECT_LOOP_SLEEP_SECONDS: u64 = 180; // 3 minutes
+
+#[derive(Debug, Error)]
+pub enum PeerDisconnectError {
+    #[error(transparent)]
+    DbError(#[from] SqlxError),
+    #[error(transparent)]
+    ModelError(#[from] ModelError),
+    #[error(transparent)]
+    WireguardError(#[from] WireguardNetworkError),
+    #[error("Failed to send gateway event: {0}")]
+    EventError(String),
+}
 
 /// Run periodic disconnect task
 ///
@@ -22,11 +39,13 @@ const DISCONNECT_LOOP_SLEEP_SECONDS: u64 = 180; // 3 minutes
 pub async fn run_periodic_peer_disconnect(
     pool: DbPool,
     wireguard_tx: Sender<GatewayEvent>,
-) -> Result<(), SqlxError> {
+) -> Result<(), PeerDisconnectError> {
     info!("Starting periodic disconnect of inactive devices in MFA-protected locations");
     loop {
+        debug!("Starting periodic inactive device disconnect");
+
         // start transaction
-        let transaction = pool.begin().await?;
+        let mut transaction = pool.begin().await?;
 
         // get all MFA-protected locations
         let locations = query_as!(
@@ -36,39 +55,61 @@ pub async fn run_periodic_peer_disconnect(
                 connected_at, mfa_enabled, keepalive_interval, peer_disconnect_threshold \
             FROM wireguard_network WHERE mfa_enabled = true",
         )
-        .fetch_all(&transaction)
+        .fetch_all(&mut *transaction)
         .await?;
 
         // loop over all locations
         for location in locations {
             debug!("Fetching inactive devices for location {location}");
+            let location_id = location.get_id()?;
             let devices = query_as!(
             Device,
             "WITH stats AS ( \
-                    SELECT DISTINCT ON (network) network, endpoint, latest_handshake \
+                    SELECT DISTINCT ON (device_id) device_id, endpoint, latest_handshake \
                     FROM wireguard_peer_stats \
-                    WHERE device_id = $2 \
-                    ORDER BY network, collected_at DESC \
+                    WHERE network = $1 \
+                    ORDER BY device_id, collected_at DESC \
                 ) \
             SELECT d.id as \"id?\", d.name, d.wireguard_pubkey, d.user_id, d.created, d.preshared_key \
             FROM device d \
             JOIN wireguard_network_device wnd ON wnd.device_id = d.id \
-            JOIN
-            WHERE wnd.wireguard_network_id = $1 AND is_authorized = true",
-            location.id
+            LEFT JOIN stats on d.id = stats.device_id \
+            WHERE wnd.wireguard_network_id = $1 AND wnd.is_authorized = true AND (NOW() - stats.latest_handshake) > $2 * interval '1 second'",
+            location_id,
+                location.peer_disconnect_threshold as f64
         )
                 .fetch_all(&mut *transaction)
                 .await?;
 
             for device in devices {
+                debug!("Processing inactive device {device}");
+                let device_id = device.get_id()?;
+
                 // get network config for device
+                if let Some(mut device_network_config) =
+                    WireguardNetworkDevice::find(&mut *transaction, device_id, location_id).await?
+                {
+                    info!("Marking device {device} as not authorized to connect to location {location}");
+                    // change `is_authorized` value for device
+                    device_network_config.is_authorized = false;
+                    device_network_config.update(&mut *transaction).await?;
 
-                // change `is_allowed` value for each device
-
-                // send `peer_delete` message to gateway
-                let event = GatewayEvent::DeviceDeleted();
-                if let Err(err) = wireguard_tx.send(event) {
-                    error!("Error sending wireguard event {err}");
+                    debug!("Sending `peer_delete` message to gateway");
+                    let device_info = DeviceInfo {
+                        device,
+                        network_info: vec![DeviceNetworkInfo {
+                            network_id: location_id,
+                            device_wireguard_ip: device_network_config.wireguard_ip,
+                        }],
+                    };
+                    let event = GatewayEvent::DeviceDeleted(device_info);
+                    wireguard_tx.send(event).map_err(|err| {
+                        error!("Error sending Wireguard event: {err}");
+                        PeerDisconnectError::EventError(err.to_string())
+                    })?;
+                } else {
+                    error!("Network config for device {device} in location {location} not found. Skipping device...");
+                    continue;
                 }
             }
         }
