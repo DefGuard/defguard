1fa4eaa5f7c0595643970de46e324af231f4675c
Maciej Wójcik <wojcik91@gmail.com>
2024-01-18 11:09:45 +0100

    formatting

--- web/src/pages/users/UserProfile/ProfileDetails/ProfileDetailsForm/ProfileDetailsForm.tsx
+++ web/src/pages/users/UserProfile/ProfileDetails/ProfileDetailsForm/ProfileDetailsForm.tsx
@@ -15,9 +15,9 @@ import useApi from '../../../../../shared/hooks/useApi';
 import { useToaster } from '../../../../../shared/hooks/useToaster';
 import { MutationKeys } from '../../../../../shared/mutations';
 import {
-    patternSafeUsernameCharacters,
-    patternValidEmail,
-    patternValidPhoneNumber,
+  patternSafeUsernameCharacters,
+  patternValidEmail,
+  patternValidPhoneNumber,
 } from '../../../../../shared/patterns';
 import { QueryKeys } from '../../../../../shared/queries';
 import { OAuth2AuthorizedApps } from '../../../../../shared/types';
