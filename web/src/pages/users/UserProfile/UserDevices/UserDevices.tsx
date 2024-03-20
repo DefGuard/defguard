b6aeccad2fd4cca9f833eda4e875ffdbcd4d18e6
Jacek Chmielewski <jchmielewski@teonite.com>
2024-03-20 08:49:50 +0100

    WIP: get networks

--- web/src/pages/users/UserProfile/UserDevices/UserDevices.tsx
+++ web/src/pages/users/UserProfile/UserDevices/UserDevices.tsx
@@ -2,9 +2,9 @@ import './style.scss';
 
 import Skeleton from 'react-loading-skeleton';
 import { useNavigate } from 'react-router';
+import { shallow } from 'zustand/shallow';
 
 import { useI18nContext } from '../../../../i18n/i18n-react';
-import { useAppStore } from '../../../../shared/hooks/store/useAppStore';
 import { useUserProfileStore } from '../../../../shared/hooks/store/useUserProfileStore';
 import { useAddDevicePageStore } from '../../../addDevice/hooks/useAddDevicePageStore';
 import { AddComponentBox } from '../../shared/components/AddComponentBox/AddComponentBox';
@@ -15,11 +15,22 @@ import { EditUserDeviceModal } from './modals/EditUserDeviceModal/EditUserDevice
 
 export const UserDevices = () => {
   const navigate = useNavigate();
-  const appInfo = useAppStore((state) => state.appInfo);
   const { LL } = useI18nContext();
   const userProfile = useUserProfileStore((state) => state.userProfile);
-  const initAddDevice = useAddDevicePageStore((state) => state.init);
+  // const [initAddDevice, networks] = useAddDevicePageStore((state) => [
+  const [initAddDevice, networks] = useAddDevicePageStore((state) => [
+    state.init,
+    state.networks,
+  ]);
 
+  // const [networks] = useAddDevicePageStore(
+  //   (state) => [
+  //     state.networks,
+  //   ],
+  //   shallow,
+  // );
+
+  console.log(networks, !networks?.length);
   return (
     <section id="user-devices">
       <header>
@@ -45,7 +56,7 @@ export const UserDevices = () => {
             <AddComponentBox
               data-testid="add-device"
               text={LL.userPage.devices.addDevice.web()}
-              disabled={!appInfo?.network_present}
+              disabled={!networks?.length}
               callback={() => {
                 initAddDevice({
                   username: userProfile.user.username,
