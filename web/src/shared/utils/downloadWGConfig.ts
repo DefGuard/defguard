8f5ad35df7b48eb340efd817536b8de8ea9787c0
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2024-06-03 11:03:37 +0200

    fix file extension on some browsers

--- web/src/shared/utils/downloadWGConfig.ts
+++ web/src/shared/utils/downloadWGConfig.ts
@@ -2,7 +2,9 @@ import saveAs from 'file-saver';
 
 export const downloadWGConfig = (config: string, fileName: string) => {
   const blob = new Blob([config.replace(/^[^\S\r\n]+|[^\S\r\n]+$/gm, '')], {
-    type: 'text/plain;charset=utf-8',
+    // octet-stream is used here as a workaround: some browsers will append 
+    // an additional .txt extension to the file name if the MIME type is text/plain.
+    type: 'application/octet-stream;charset=utf-8',
   });
   saveAs(blob, `${fileName.toLowerCase()}.conf`);
 };
