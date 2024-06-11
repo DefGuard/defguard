474a02e7464550b1ace2dbcffae6072181343c23
Jacek Chmielewski <jchmielewski@teonite.com>
2024-06-11 10:52:06 +0200

    wip: try and parse the claims
    ISSUE: no jwks keys

--- src/handlers/openid_login.rs
+++ src/handlers/openid_login.rs
@@ -3,7 +3,10 @@ use std::str::FromStr;
 use crate::{error::WebError, AppState};
 use axum::extract::{Query, State};
 use openidconnect::{
-    core::CoreIdTokenVerifier, AdditionalClaims, GenderClaim, IdToken, IdTokenVerifier, JsonWebKeyType, JweContentEncryptionAlgorithm, JwsSigningAlgorithm
+    core::{CoreClient, CoreIdTokenVerifier, CoreProviderMetadata},
+    AdditionalClaims, AuthUrl, ClientId, GenderClaim, IdToken, IdTokenVerifier, IssuerUrl,
+    JsonWebKeySet, JsonWebKeyType, JweContentEncryptionAlgorithm, JwsSigningAlgorithm,
+    NonceVerifier,
 };
 
 // /// Authorization Endpoint
@@ -25,6 +28,14 @@ pub struct AuthenticationResponse {
     id_token: String,
 }
 
+struct Verifier;
+
+impl NonceVerifier for Verifier {
+    fn verify(self, nonce: Option<&openidconnect::Nonce>) -> Result<(), String> {
+        Ok(())
+    }
+}
+
 pub async fn auth_callback(
     State(state): State<AppState>,
     Query(response): Query<AuthenticationResponse>,
@@ -43,6 +54,40 @@ pub async fn auth_callback(
         // TOOD(jck): create user based on user info
         // TOOD(jck): log user in
         // token.claims(verifier, nonce_verifier);
+        // let provider_metadata = CoreProviderMetadata::discover(
+        //     &IssuerUrl::new("https://accounts.example.com".to_string())?,
+        //     http_client,
+        // )?;
+
+        let client_id = ClientId::new("f2cef8b3-5b09-4c3f-988b-51fc3c42ecbc".to_string());
+        let client_secret = None;
+        let issuer_url = IssuerUrl::new(
+            "https://login.microsoftonline.com/2fc43015-5699-4d01-bd01-d6f2bd66818a/v2.0"
+                .to_string(),
+        )
+        .unwrap();
+        let auth_url = AuthUrl::new(
+            "https://login.microsoftonline.com/2fc43015-5699-4d01-bd01-d6f2bd66818a/oauth2/v2.0/authorize".to_string()
+        ).unwrap();
+        let token_url = None;
+        let userinfo_url = None;
+        let jwks = JsonWebKeySet::default();
+        let client = CoreClient::new(
+            client_id,
+            client_secret,
+            issuer_url,
+            auth_url,
+            token_url,
+            userinfo_url,
+            jwks,
+        );
+        let claims = token.claims(&client.id_token_verifier(), Verifier);
+        info!("### Decoded claims: {claims:#?}");
+        // let name = claims.name().unwrap();
+        // let family_name = claims.family_name().unwrap();
+        // let given_name = claims.given_name().unwrap();
+        // let email = claims.email().unwrap();
+        // info!("### name: {name:?}, family_name: {family_name:?}, given_name: {given_name:?}, email: {email:?}");
 
         // TODO(jck): log all useful stuff
         info!("OIDC login succeeded for user");
