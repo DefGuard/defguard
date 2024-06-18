9ae4a6228f50f1222b24183e1ebcac31668298b4
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2024-06-18 15:15:05 +0200

    feat: add new logo (#646)
    * add new logo
    
    * update pwa icons
    
    * optimize with oxipng
    
    * fix appstore logo

---
+++ web/src/shared/components/svg/DefguardNavLogoCollapsed.tsx
@@ -0,0 +1,24 @@
+import type { SVGProps } from 'react';
+const SvgDefguardNavLogoCollapsed = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={21}
+    height={43}
+    fill="none"
+    viewBox="0 0 21 43"
+    {...props}
+  >
+    <g clipPath="url(#defguard-nav-logo-collapsed_svg__a)">
+      <path
+        fill="#0C8CE0"
+        d="M18.706 0v9.315l-8.178-4.658L0 10.65v22.625l10.52 5.992 8.178-4.658v4.8l-3.957 2.258L17.08 43l3.957-2.257V21.29L10.52 15.298 2.34 19.956v-7.98l8.18-4.658 8.178 4.657v2.652l2.34 1.334V1.334L18.698 0zM2.34 31.94v-7.981l8.18 4.657 8.178-4.657v7.98l-8.179 4.658zm15.192-9.978-7.013 3.994-7.013-3.994 7.013-3.995z"
+      />
+    </g>
+    <defs>
+      <clipPath id="defguard-nav-logo-collapsed_svg__a">
+        <path fill="#fff" d="M0 0h21v43H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgDefguardNavLogoCollapsed;
