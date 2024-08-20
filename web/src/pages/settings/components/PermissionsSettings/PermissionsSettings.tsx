1c8fa423888b5dad9a70d4f65b69574116128c6d
Jacek Chmielewski <jchmielewski@teonite.com>
2024-08-20 09:54:36 +0200

    Enterprise-only behaviour tab

--- web/src/pages/settings/components/PermissionsSettings/PermissionsSettings.tsx
+++ web/src/pages/settings/components/PermissionsSettings/PermissionsSettings.tsx
@@ -1,10 +1,33 @@
+import { useI18nContext } from '../../../../i18n/i18n-react';
+import { useAppStore } from '../../../../shared/hooks/store/useAppStore';
 import { PermissionsForm } from './components/PermissionsForm';
 
-export const PermissionsSettings = () => (
-  <>
-    <div className="left">
-      <PermissionsForm />
-    </div>
-    <div className="right"></div>
-  </>
-);
+export const PermissionsSettings = () => {
+  const enterpriseEnabled = useAppStore((state) => state.enterprise_enabled);
+  const { LL } = useI18nContext();
+  const localLL = LL.settingsPage.enterpriseOnly;
+  return (
+    <>
+      {!enterpriseEnabled && (
+        <div className="enterprise-info-backdrop">
+          <div className="enterprise-info">
+            <div>
+              <h2>{localLL.title()}</h2>
+              <p>
+                {localLL.subtitle()}{' '}
+                <a href="https://defguard.net/pricing/" target="_blank" rel="noreferrer">
+                  {localLL.website()}
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="left">
+        <PermissionsForm />
+      </div>
+      <div className="right"></div>
+    </>
+  );
+};
