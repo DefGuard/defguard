d4a7a9046afb9c96dbd7df41385b7cd71e01d9b6
Adam Ciarciński <aciarcinski@teonite.com>
2024-11-12 12:54:27 +0100

    Add migration to make openid_sub unique

---
+++ migrations/20241112105513_openid_sub_unique.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "user" DROP CONSTRAINT "user_openid_sub_key";
---
+++ migrations/20241112105513_openid_sub_unique.up.sql
@@ -0,0 +1,3 @@
+-- Make openid_sub unique.
+-- This migration may fail if duplicate openid_subs exist in the database.
+ALTER TABLE "user" ADD CONSTRAINT "user_openid_sub_key" UNIQUE (openid_sub);
