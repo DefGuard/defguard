75f776426e8a1df79e4ab8ffd12c64e4ef2106a4
Adam Ciarciński <aciarcinski@teonite.com>
2024-12-02 12:23:02 +0100

    Fix patternSafeUsernameCharacters

--- web/src/shared/patterns.ts
+++ web/src/shared/patterns.ts
@@ -68,7 +68,7 @@ export const patternValidUrl = new RegExp(
 export const patternValidDomain =
   /^(?:(?:(?:[a-zA-z\-]+)\:\/{1,3})?(?:[a-zA-Z0-9])(?:[a-zA-Z0-9\-\.]){1,61}(?:\.[a-zA-Z]{2,})+|\[(?:(?:(?:[a-fA-F0-9]){1,4})(?::(?:[a-fA-F0-9]){1,4}){7}|::1|::)\]|(?:(?:[0-9]{1,3})(?:\.[0-9]{1,3}){3}))(?:\:[0-9]{1,5})?$/;
 
-export const patternSafeUsernameCharacters = /^[a-zA-Z0-9]+[a-zA-Z0-9.\-_]+$/;
+export const patternSafeUsernameCharacters = /^[a-zA-Z0-9]+[a-zA-Z0-9.\-_]*$/;
 
 export const patternSafePasswordCharacters =
   /^[a-zA-Z0-9.!@#$%^&*()_+\-=\[\]{}|,<>\/?~]+$/;
