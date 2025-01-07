a823c5289ea249d62399532d6035f4966b31dd99
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2025-01-07 12:39:23 +0100

    change endpoint response structure

--- src/handlers/app_info.rs
+++ src/handlers/app_info.rs
@@ -12,18 +12,23 @@ use crate::{
     },
 };
 
-/// Additional information about core state.
 #[derive(Serialize)]
-pub struct AppInfo {
-    version: String,
-    network_present: bool,
-    smtp_enabled: bool,
+struct LicenseInfo {
     /// Whether the enterprise features are enabled.
     enterprise: bool,
     /// Which limits are exceeded.
     limits_exceeded: LimitsExceeded,
     /// Is any of the limits exceeded.
     any_limit_exceeded: bool,
+}
+
+/// Additional information about core state.
+#[derive(Serialize)]
+pub struct AppInfo {
+    version: String,
+    network_present: bool,
+    smtp_enabled: bool,
+    license_info: LicenseInfo,
 }
 
 pub(crate) async fn get_app_info(
@@ -34,14 +39,16 @@ pub(crate) async fn get_app_info(
     let settings = Settings::get_settings(&appstate.pool).await?;
     let enterprise = is_enterprise_enabled();
     let limits_exceeded = get_counts().get_exceeded_limits();
-    let any_exceeded = limits_exceeded.any();
+    let any_limit_exceeded = limits_exceeded.any();
     let res = AppInfo {
         network_present: !networks.is_empty(),
         smtp_enabled: settings.smtp_configured(),
         version: VERSION.into(),
-        enterprise,
-        limits_exceeded,
-        any_limit_exceeded: any_exceeded,
+        license_info: LicenseInfo {
+            enterprise,
+            limits_exceeded,
+            any_limit_exceeded,
+        },
     };
 
     Ok(ApiResponse::new(json!(res), StatusCode::OK))
