ffa8f5fcc1b751e52509d36d2fe11f94f77769dc
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2025-01-10 14:35:45 +0100

    eslint

--- web/src/shared/hooks/store/useAppStore.ts
+++ web/src/shared/hooks/store/useAppStore.ts
@@ -3,12 +3,7 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 import { createWithEqualityFn } from 'zustand/traditional';
 
 import { Locales } from '../../../i18n/i18n-types';
-import {
-  AppInfo,
-  EnterpriseStatus,
-  SettingsEnterprise,
-  SettingsEssentials,
-} from '../../types';
+import { AppInfo, SettingsEnterprise, SettingsEssentials } from '../../types';
 
 const defaultValues: StoreValues = {
   settings: undefined,
