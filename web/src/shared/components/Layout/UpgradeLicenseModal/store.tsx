d92547159385ee390a08f173e36c4593fa1601b1
Filip Ślęzak <fslezak@teonite.com>
2025-01-10 12:03:00 +0100

    make license limit modals open with time limit

--- web/src/shared/components/Layout/UpgradeLicenseModal/store.tsx
+++ web/src/shared/components/Layout/UpgradeLicenseModal/store.tsx
@@ -1,21 +1,55 @@
+import dayjs from 'dayjs';
+import { persist } from 'zustand/middleware';
 import { createWithEqualityFn } from 'zustand/traditional';
 
 import { UpgradeLicenseModalVariant } from './types';
 
+//minutes
+const modalTimeout = 30;
+
 const defaults: StoreValues = {
   visible: false,
   modalVariant: UpgradeLicenseModalVariant.ENTERPRISE_NOTICE,
+  lastClosed: undefined,
 };
 
-export const useUpgradeLicenseModal = createWithEqualityFn<Store>(
-  (set) => ({
-    ...defaults,
-    open: ({ modalVariant }) => {
-      set({ visible: true, modalVariant });
+export const useUpgradeLicenseModal = createWithEqualityFn<Store>()(
+  persist(
+    (set, get) => ({
+      ...defaults,
+      open: ({ modalVariant }) => {
+        const { lastClosed } = get();
+        if (
+          lastClosed !== undefined &&
+          modalVariant === UpgradeLicenseModalVariant.LICENSE_LIMIT
+        ) {
+          const past = dayjs(lastClosed).utc();
+          const now = dayjs().utc();
+          const diff = now.diff(past, 'minutes');
+          if (diff >= modalTimeout) {
+            set({ visible: true, modalVariant });
+          }
+        } else {
+          set({ visible: true, modalVariant });
+        }
+      },
+      close: () => {
+        const { modalVariant } = get();
+        if (modalVariant === UpgradeLicenseModalVariant.LICENSE_LIMIT) {
+          set({ visible: false, lastClosed: dayjs().utc().toISOString() });
+        } else {
+          set({ visible: false });
+        }
+      },
+      reset: () =>
+        set({ visible: defaults.visible, modalVariant: defaults.modalVariant }),
+    }),
+    {
+      name: 'upgrade-license-modal',
+      version: 1,
+      partialize: (s) => ({ lastOpened: s.lastClosed }),
     },
-    close: () => set({ visible: false }),
-    reset: () => set({ visible: defaults.visible, modalVariant: defaults.modalVariant }),
-  }),
+  ),
   Object.is,
 );
 
@@ -23,6 +57,7 @@ type Store = StoreValues & StoreMethods;
 type StoreValues = {
   visible: boolean;
   modalVariant: UpgradeLicenseModalVariant;
+  lastClosed?: string;
 };
 type Open = Pick<StoreValues, 'modalVariant'>;
 
