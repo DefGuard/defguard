a9647c9364c596b0f9939ede2e8bdc0efb809693
Jacek Chmielewski <jchmielewski@teonite.com>
2025-02-27 11:08:30 +0100

    ACL API (#1008)
    * Handle all related objects durion ACL rule creation
    * Update ACL rule API enpoint
    * Delete ACL rule API endpoint
    * GET ACL rule API endpoint
    * Add devices to AclRule
    * Refactor get allowed / denied objects methods
    * AclRuleDestinationRange model
    * Add & delete destination ranges
    * Range order check
    * add destination ranges to AclAlias
    * Universal conversions for port ranges
    * Full Alias API
    * Move AclRuleDestinationRangeInfo to model module
    * PUT requests return status 200; fix PortRange <-> PgRange conversions
    * Update sqlx fixtures
    * Fix clippy issues
    * Add bounded_ports check constraint on acl rule and alias; remove created_at from alias
    * Panic on unbounded port range
    * Update sqlx fixtures

--- migrations/20250213094023_acl.down.sql
+++ migrations/20250213094023_acl.down.sql
@@ -1,6 +1,10 @@
+DROP TABLE aclaliasdestinationrange;
+DROP TABLE aclruledestinationrange;
+DROP TABLE aclruledevice;
 DROP TABLE aclrulealias;
 DROP TABLE aclrulenetwork;
 DROP TABLE aclruleuser;
 DROP TABLE aclrulegroup;
 DROP TABLE aclalias;
 DROP TABLE aclrule;
+DROP FUNCTION all_ranges_bounded;
