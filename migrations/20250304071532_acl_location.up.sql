5efe4ce62fdef9b74c2638388dfe4eb4a500d2fb
Jacek Chmielewski <jchmielewski@teonite.com>
2025-03-04 10:30:45 +0100

    Sqlx fixtures

--- migrations/20250304071532_acl_location.up.sql
+++ migrations/20250304071532_acl_location.up.sql
@@ -1,4 +1,3 @@
 ALTER TABLE wireguard_network ADD COLUMN acl_enabled boolean NOT NULL default false;
 ALTER TABLE wireguard_network ADD COLUMN acl_default_allow boolean NOT NULL default false;
 ALTER TABLE aclrule ADD COLUMN enabled boolean NOT NULL default true;
-
