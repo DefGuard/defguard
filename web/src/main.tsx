17a3b2bb18052961bce1b5f02bde2ef06063b7da
Filip Ślęzak <filipslezaklab@gmail.com>
2025-03-17 16:14:58 +0100

    disable query devtools

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,6 @@ import './shared/scss/styles.scss';
 import './shared/defguard-ui/scss/index.scss';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import dayjs from 'dayjs';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 import utc from 'dayjs/plugin/utc';
@@ -23,7 +22,6 @@ root.render(
       <TranslationProvider>
         <AppLoader />
       </TranslationProvider>
-      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </StrictMode>,
 );
