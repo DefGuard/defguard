b89adbd3877e0d0b6ebdac9967bd92fd3e9d3f1e
Maciej Wójcik <maciek@wjck.pl>
2025-03-21 18:32:39 +0100

    fix missing default in migration

--- migrations/20250321110001_add_allow_deny_all_network_devices.up.sql
+++ migrations/20250321110001_add_allow_deny_all_network_devices.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE aclrule ADD COLUMN allow_all_network_devices boolean NOT NULL;
-ALTER TABLE aclrule ADD COLUMN deny_all_network_devices boolean NOT NULL;
+ALTER TABLE aclrule ADD COLUMN allow_all_network_devices boolean NOT NULL DEFAULT false;
+ALTER TABLE aclrule ADD COLUMN deny_all_network_devices boolean NOT NULL DEFAULT false;
