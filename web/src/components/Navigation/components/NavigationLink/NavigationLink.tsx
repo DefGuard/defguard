00a2eabff96b353ca67a8cf28b7da6b3489c9bbe
Maciej Wójcik <maciek@wjck.pl>
2025-03-27 15:33:10 +0100

    display upgrade modal when accessing ACL page without license

--- web/src/components/Navigation/components/NavigationLink/NavigationLink.tsx
+++ web/src/components/Navigation/components/NavigationLink/NavigationLink.tsx
@@ -3,7 +3,11 @@ import './style.scss';
 import classNames from 'classnames';
 import { useMatch } from 'react-router';
 import { Link } from 'react-router-dom';
+import { shallow } from 'zustand/shallow';
 
+import { useUpgradeLicenseModal } from '../../../../shared/components/Layout/UpgradeLicenseModal/store';
+import { UpgradeLicenseModalVariant } from '../../../../shared/components/Layout/UpgradeLicenseModal/types';
+import { useAppStore } from '../../../../shared/hooks/store/useAppStore';
 import { NavigationItem } from '../../types';
 
 interface NavigationLinkProps {
@@ -12,6 +16,8 @@ interface NavigationLinkProps {
 }
 
 export const NavigationLink = ({ item, callback }: NavigationLinkProps) => {
+  const openUpgradeLicenseModal = useUpgradeLicenseModal((s) => s.open, shallow);
+  const enterpriseEnabled = useAppStore((s) => s.appInfo?.license_info.enterprise);
   const match = useMatch(item.linkPath);
 
   const cn = classNames(
@@ -27,7 +33,13 @@ export const NavigationLink = ({ item, callback }: NavigationLinkProps) => {
       replace
       to={item.linkPath}
       className={cn}
-      onClick={() => {
+      onClick={(event) => {
+        if (item.enterpriseOnly && !enterpriseEnabled) {
+          event.preventDefault();
+          openUpgradeLicenseModal({
+            modalVariant: UpgradeLicenseModalVariant.ENTERPRISE_NOTICE,
+          });
+        }
         if (callback) {
           callback();
         }
