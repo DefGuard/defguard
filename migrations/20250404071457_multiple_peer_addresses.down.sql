46e979bd4c9bd1c97b4824aafcd4dfe765366233
Jacek Chmielewski <jchmielewski@teonite.com>
2025-04-04 09:29:12 +0200

    migrations for multi-address peers

---
+++ migrations/20250404071457_multiple_peer_addresses.down.sql
@@ -0,0 +1,16 @@
+-- add old-type address column
+ALTER TABLE wireguard_network_device
+ADD COLUMN wireguard_ip_old inet;
+
+-- copy the first element of new column to old column
+-- all further addresses will be lost
+UPDATE wireguard_network_device
+SET wireguard_ip_old = wireguard_ip[1];
+
+-- drop the "new" column
+ALTER TABLE wireguard_network_device
+DROP COLUMN wireguard_ip;
+
+-- rename the column
+ALTER TABLE wireguard_network_device
+RENAME COLUMN wireguard_ip_old TO wireguard_ip;
