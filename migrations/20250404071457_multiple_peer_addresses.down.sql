a76e8a2b759de7002fff93adf14c4b8a88377f49
Jacek Chmielewski <jchmielewski@teonite.com>
2025-04-11 08:30:04 +0200

    fix downmigration

--- migrations/20250404071457_multiple_peer_addresses.down.sql
+++ migrations/20250404071457_multiple_peer_addresses.down.sql
@@ -1,6 +1,6 @@
 -- add old-type address column
 ALTER TABLE wireguard_network_device
-ADD COLUMN wireguard_ip_old inet;
+ADD COLUMN wireguard_ip_old inet NOT NULL;
 
 -- copy the first element of new column to old column
 -- all further addresses will be lost
