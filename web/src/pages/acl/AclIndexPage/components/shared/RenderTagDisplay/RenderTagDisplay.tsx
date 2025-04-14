e62023ec8d09aaea78a10b6269007ac66118dc62
Filip Ślęzak <fslezak@teonite.com>
2025-04-14 14:57:44 +0200

    resolve acl's todos

--- web/src/pages/acl/AclIndexPage/components/shared/RenderTagDisplay/RenderTagDisplay.tsx
+++ web/src/pages/acl/AclIndexPage/components/shared/RenderTagDisplay/RenderTagDisplay.tsx
@@ -1,21 +1,32 @@
 import './style.scss';
 
+import clsx from 'clsx';
+
 import { FloatingMenu } from '../../../../../../shared/defguard-ui/components/Layout/FloatingMenu/FloatingMenu';
 import { FloatingMenuProvider } from '../../../../../../shared/defguard-ui/components/Layout/FloatingMenu/FloatingMenuProvider';
 import { FloatingMenuTrigger } from '../../../../../../shared/defguard-ui/components/Layout/FloatingMenu/FloatingMenuTrigger';
 import { Tag } from '../../../../../../shared/defguard-ui/components/Layout/Tag/Tag';
+import { isPresent } from '../../../../../../shared/defguard-ui/utils/isPresent';
 import { ListTagDisplay } from '../types';
 
 type RenderTagsProps = {
   data: ListTagDisplay[];
+  placeholder?: string;
 };
 
-export const RenderTagDisplay = ({ data }: RenderTagsProps) => {
+export const RenderTagDisplay = ({ data, placeholder }: RenderTagsProps) => {
   return (
-    <FloatingMenuProvider placement="right">
+    <FloatingMenuProvider placement="right" disabled={data.length === 0}>
       <FloatingMenuTrigger asChild>
-        <div className="tags-display">
+        <div
+          className={clsx('tags-display', {
+            empty: data.length === 0,
+          })}
+        >
           <TagContent data={data} />
+          {data.length === 0 && isPresent(placeholder) && (
+            <p className="placeholder">{placeholder}</p>
+          )}
         </div>
       </FloatingMenuTrigger>
       <FloatingMenu>
