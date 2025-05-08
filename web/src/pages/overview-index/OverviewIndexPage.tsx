1a3a0db356f66e2571abd118486a5b01daaf12dd
Filip Ślęzak <fslezak@teonite.com>
2025-05-08 10:41:59 +0200

    index up

--- web/src/pages/overview-index/OverviewIndexPage.tsx
+++ web/src/pages/overview-index/OverviewIndexPage.tsx
@@ -1,9 +1,58 @@
 import './style.scss';
 
+import { useQuery } from '@tanstack/react-query';
+import { useCallback, useMemo } from 'react';
+
 import { ExpandableSection } from '../../shared/components/Layout/ExpandableSection/ExpandableSection';
 import { PageContainer } from '../../shared/components/Layout/PageContainer/PageContainer';
+import useApi from '../../shared/hooks/useApi';
+import { Network, WireguardNetworkStats } from '../../shared/types';
+import { sumBy } from 'lodash-es';
+
+type OverviewIndexNetworkStats = Network & {
+  stats: WireguardNetworkStats;
+};
 
 export const OverviewIndexPage = () => {
+  const {
+    network: { getNetworks, getNetworkStats },
+  } = useApi();
+
+  const query = useCallback(async () => {
+    const res: OverviewIndexNetworkStats[] = [];
+    const networks = await getNetworks();
+    const stats: WireguardNetworkStats[] = [];
+    for (const network of networks) {
+      const stats = await getNetworkStats({
+        id: network.id,
+      });
+      res.push({
+        ...network,
+        stats,
+      });
+    }
+    return stats;
+  }, [getNetworkStats, getNetworks]);
+
+  const { data } = useQuery({
+    queryKey: ['overview-index'],
+    queryFn: query,
+  });
+
+  const summaryData = useMemo(() => {
+    if (!data) return undefined;
+    const res: WireguardNetworkStats = {
+      active_devices: sumBy(data, 'active_devices'),
+      active_users: sumBy(data, 'active_users'),
+      current_active_devices: sumBy(data, 'current_active_devices'),
+      current_active_users: sumBy(data, 'current_active_users'),
+      download: sumBy(data, 'download'),
+      upload: sumBy(data, 'upload'),
+      transfer_series: [],
+    };
+    return res;
+  }, [data]);
+
   return (
     <PageContainer id="overview-index">
       <div>
