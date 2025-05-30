ce9d3ebfdea8dea256a61f2380c6609fe0392b83
Jacek Chmielewski <jchmielewski@teonite.com>
2025-05-30 08:31:21 +0200

    Refactor handle_api_event method

--- crates/defguard_event_router/src/handlers/api.rs
+++ crates/defguard_event_router/src/handlers/api.rs
@@ -8,82 +8,40 @@ impl EventRouter {
     pub(crate) fn handle_api_event(&self, event: ApiEvent) -> Result<(), EventRouterError> {
         debug!("Processing API event: {event:?}");
 
-        match event.kind {
-            ApiEventType::UserLogin => {
-                // send event to audit log
-                self.log_event(
-                    event.context,
-                    LoggerEvent::Defguard(DefguardEvent::UserLogin),
-                )?;
-            }
-            ApiEventType::UserLogout => {
-                self.log_event(
-                    event.context,
-                    LoggerEvent::Defguard(DefguardEvent::UserLogout),
-                )?;
-            }
-            ApiEventType::UserAdded { username } => {
-                self.log_event(
-                    event.context,
-                    LoggerEvent::Defguard(DefguardEvent::UserAdded { username }),
-                )?;
-            }
-            ApiEventType::UserRemoved { username } => {
-                self.log_event(
-                    event.context,
-                    LoggerEvent::Defguard(DefguardEvent::UserRemoved { username }),
-                )?;
-            }
-            ApiEventType::UserModified { username } => {
-                self.log_event(
-                    event.context,
-                    LoggerEvent::Defguard(DefguardEvent::UserModified { username }),
-                )?;
-            }
+        let logger_event = match event.kind {
+            ApiEventType::UserLogin => LoggerEvent::Defguard(DefguardEvent::UserLogin),
+            ApiEventType::UserLogout => LoggerEvent::Defguard(DefguardEvent::UserLogout),
+            ApiEventType::UserAdded { username } => LoggerEvent::Defguard(DefguardEvent::UserAdded { username }),
+            ApiEventType::UserRemoved { username } => LoggerEvent::Defguard(DefguardEvent::UserRemoved { username }),
+            ApiEventType::UserModified { username } => LoggerEvent::Defguard(DefguardEvent::UserModified { username }),
             ApiEventType::UserDeviceAdded {
                 owner,
                 device_id,
                 device_name,
-            } => {
-                self.log_event(
-                    event.context,
-                    LoggerEvent::Defguard(DefguardEvent::UserDeviceAdded {
-                        device_name,
-                        device_id,
-                        owner,
-                    }),
-                )?;
-            }
+            } => LoggerEvent::Defguard(DefguardEvent::UserDeviceAdded {
+                    device_name,
+                    device_id,
+                    owner,
+                }),
             ApiEventType::UserDeviceRemoved {
                 owner,
                 device_id,
                 device_name,
-            } => {
-                self.log_event(
-                    event.context,
-                    LoggerEvent::Defguard(DefguardEvent::UserDeviceRemoved {
-                        device_name,
-                        device_id,
-                        owner,
-                    }),
-                )?;
-            }
+            } => LoggerEvent::Defguard(DefguardEvent::UserDeviceRemoved {
+                device_name,
+                device_id,
+                owner,
+            }),
             ApiEventType::UserDeviceModified {
                 owner,
                 device_id,
                 device_name,
-            } => {
-                self.log_event(
-                    event.context,
-                    LoggerEvent::Defguard(DefguardEvent::UserDeviceModified {
-                        device_name,
-                        device_id,
-                        owner,
-                    }),
-                )?;
-            }
-        }
-
-        Ok(())
+            } => LoggerEvent::Defguard(DefguardEvent::UserDeviceModified {
+                device_name,
+                device_id,
+                owner,
+            }),
+        };
+        self.log_event( event.context, logger_event)
     }
 }
