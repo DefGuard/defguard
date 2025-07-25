8b4e5934e423c4db92b92a80c78d9d95f56b351c
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2025-07-25 11:41:40 +0200

    build for multiple regions

--- images/ami/core.sh
+++ images/ami/core.sh
@@ -4,13 +4,6 @@ set -e
 echo "Updating apt repositories..."
 sudo apt update
 
-# echo "Installing curl..."
-# sudo apt install -y curl
-
-# DEB_URL="https://github.com/DefGuard/defguard/releases/download/v${PACKAGE_VERSION}/defguard-${PACKAGE_VERSION}-x86_64-unknown-linux-gnu.deb"
-# echo "Downloading Defguard package from: $DEB_URL"
-# sudo curl -fsSL -o /tmp/defguard-core.deb "$DEB_URL"
-
 echo "Installing Defguard package..."
 sudo dpkg -i /tmp/defguard-core.deb
 
