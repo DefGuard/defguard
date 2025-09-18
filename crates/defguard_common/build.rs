93697898a32004ae3156845d593d657f65a927c3
Maciej Wójcik <maciej@defguard.net>
2025-09-18 15:20:04 +0200

    remove commented out code

--- crates/defguard_common/build.rs
+++ crates/defguard_common/build.rs
@@ -5,47 +5,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let git2 = Git2Builder::default().branch(true).sha(true).build()?;
     Emitter::default().add_instructions(&git2)?.emit()?;
 
-    // tonic_prost_build::configure()
-    //     // These types contain sensitive data.
-    //     .skip_debug([
-    //         "ActivateUserRequest",
-    //         "AuthInfoResponse",
-    //         "AuthenticateRequest",
-    //         "AuthenticateResponse",
-    //         "ClientMfaFinishResponse",
-    //         "CodeMfaSetupStartResponse",
-    //         "CodeMfaSetupFinishResponse",
-    //         "CoreRequest",
-    //         "CoreResponse",
-    //         "DeviceConfigResponse",
-    //         "InstanceInfoResponse",
-    //         "NewDevice",
-    //         "PasswordResetRequest",
-    //     ])
-    //     .protoc_arg("--experimental_allow_proto3_optional")
-    //     .type_attribute(
-    //         "LicenseLimits",
-    //         "#[derive(serde::Serialize, serde::Deserialize)]",
-    //     )
-    //     .compile_protos(
-    //         &[
-    //             "../../proto/core/auth.proto",
-    //             "../../proto/core/proxy.proto",
-    //             "../../proto/worker/worker.proto",
-    //             "../../proto/wireguard/gateway.proto",
-    //             "../../proto/enterprise/firewall/firewall.proto",
-    //             "src/enterprise/proto/license.proto",
-    //         ],
-    //         &[
-    //             "../../proto/core",
-    //             "../../proto/worker",
-    //             "../../proto/wireguard",
-    //             "../../proto/enterprise/firewall",
-    //             "src/enterprise/proto",
-    //         ],
-    //     )?;
     println!("cargo:rerun-if-changed=../../migrations");
-    // println!("cargo:rerun-if-changed=../../proto");
-    // println!("cargo:rerun-if-changed=src/enterprise");
     Ok(())
 }
