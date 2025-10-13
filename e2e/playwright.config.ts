3a9a19c94b0767ffdf6df90565f1e7160a4e99c6
Filip Ślęzak <fslezak@teonite.com>
2025-10-13 15:36:59 +0200

    add max failures

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  maxFailures: process.env.CI ? 1 : undefined,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: reporter,
