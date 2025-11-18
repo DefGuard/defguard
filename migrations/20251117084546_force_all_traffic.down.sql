e61e5e4b76d27b81c7f5775751e23569a6f4189f
Jacek Chmielewski <jacek@defguard.net>
2025-11-18 10:03:55 +0100

    client traffic policy enum - migration

--- migrations/20251117084546_force_all_traffic.down.sql
+++ migrations/20251117084546_force_all_traffic.down.sql
@@ -1,1 +1,16 @@
-ALTER TABLE enterprisesettings DROP COLUMN force_all_traffic;
+-- restore boolean `mfa_enabled` column
+ALTER TABLE enterprisesettings ADD COLUMN "disable_all_traffic" BOOLEAN DEFAULT false;
+
+-- populate based on client traffic policy
+UPDATE enterprisesettings
+SET disable_all_traffic = CASE
+    WHEN client_traffic_policy = 'disable_all_traffic'::client_traffic_policy THEN true
+    ELSE false
+END;
+
+-- make the restored column NOT NULL
+ALTER TABLE enterprisesettings ALTER COLUMN "disable_all_traffic" SET NOT NULL;
+
+-- drop new column and type
+ALTER TABLE enterprisesettings DROP COLUMN "client_traffic_policy";
+DROP TYPE client_traffic_policy;
--- migrations/20251117084546_force_all_traffic.up.sql
+++ migrations/20251117084546_force_all_traffic.up.sql
@@ -1,1 +1,22 @@
-ALTER TABLE enterprisesettings ADD COLUMN force_all_traffic BOOLEAN NOT NULL DEFAULT FALSE;
+-- add enum representing client traffic policy
+CREATE TYPE client_traffic_policy AS ENUM (
+    'none',
+    'disable_all_traffic',
+    'force_all_traffic'
+);
+
+-- add nullable column to `enterprisesettings` table
+ALTER TABLE enterprisesettings ADD COLUMN "client_traffic_policy" client_traffic_policy DEFAULT 'none';
+
+-- populate new column based on value in `disable_all_traffic` column
+UPDATE enterprisesettings
+SET client_traffic_policy = CASE
+    WHEN disable_all_traffic = true THEN 'disable_all_traffic'::client_traffic_policy
+    ELSE 'none'::client_traffic_policy
+END;
+
+-- make new column NOT NULL
+ALTER TABLE enterprisesettings ALTER COLUMN "client_traffic_policy" SET NOT NULL;
+
+-- drop the `disable_all_traffic` column since it's no longer needed
+ALTER TABLE enterprisesettings DROP COLUMN "disable_all_traffic";
