02e30e6e52f55e4552f8774d6a250a50cde1169a
Jacek Chmielewski <jacek@defguard.net>
2025-11-19 13:28:33 +0100

    tweak migrations

--- migrations/20251119122424_client_traffic_policy.up.sql
+++ migrations/20251119122424_client_traffic_policy.up.sql
@@ -5,8 +5,8 @@ CREATE TYPE client_traffic_policy AS ENUM (
     'force_all_traffic'
 );
 
--- add nullable column to `enterprisesettings` table
-ALTER TABLE enterprisesettings ADD COLUMN "client_traffic_policy" client_traffic_policy DEFAULT 'none';
+-- add column to `enterprisesettings` table
+ALTER TABLE enterprisesettings ADD COLUMN "client_traffic_policy" client_traffic_policy NOT NULL DEFAULT 'none';
 
 -- populate new column based on value in `disable_all_traffic` column
 UPDATE enterprisesettings
@@ -14,9 +14,6 @@ SET client_traffic_policy = CASE
     WHEN disable_all_traffic = true THEN 'disable_all_traffic'::client_traffic_policy
     ELSE 'none'::client_traffic_policy
 END;
-
--- make new column NOT NULL
-ALTER TABLE enterprisesettings ALTER COLUMN "client_traffic_policy" SET NOT NULL;
 
 -- drop the `disable_all_traffic` column since it's no longer needed
 ALTER TABLE enterprisesettings DROP COLUMN "disable_all_traffic";
