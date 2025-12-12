ce7590bcdc046156194e61200e2ab453fba42443
Maciej Wójcik <maciej@defguard.net>
2025-12-12 16:25:32 +0100

    add tests

--- crates/defguard_core/src/enterprise/mod.rs
+++ crates/defguard_core/src/enterprise/mod.rs
@@ -65,8 +65,90 @@ pub(crate) fn is_enterprise_free() -> bool {
 
 #[cfg(test)]
 mod test {
+    use chrono::{TimeDelta, Utc};
+
+    use crate::{
+        enterprise::{
+            is_business_license_active, is_enterprise_license_active,
+            license::{License, LicenseTier, set_cached_license},
+            limits::{Counts, set_counts},
+        },
+        grpc::proto::enterprise::license::LicenseLimits,
+    };
+
     #[test]
     fn test_feature_gates_no_license() {
-        todo!()
+        set_cached_license(None);
+
+        // free limits are not exceeded
+        let counts = Counts::new(1, 1, 1, 1);
+        set_counts(counts);
+
+        assert!(is_business_license_active());
+        assert!(is_enterprise_license_active());
+
+        // exceed free limits
+        let counts = Counts::new(1, 1, 5, 1);
+        set_counts(counts);
+
+        assert!(!is_business_license_active());
+        assert!(!is_enterprise_license_active());
+    }
+
+    #[test]
+    fn test_feature_gates_with_license() {
+        // exceed free limits
+        let counts = Counts::new(1, 1, 5, 1);
+        set_counts(counts);
+
+        // set Business license
+        let users_limit = 15;
+        let devices_limit = 35;
+        let locations_limit = 5;
+        let network_devices_limit = 10;
+
+        let limits = LicenseLimits {
+            users: users_limit,
+            devices: devices_limit,
+            locations: locations_limit,
+            network_devices: Some(network_devices_limit),
+        };
+        let license = License::new(
+            "test".to_string(),
+            true,
+            Some(Utc::now() + TimeDelta::days(1)),
+            Some(limits),
+            None,
+            LicenseTier::Business,
+        );
+        set_cached_license(Some(license));
+
+        assert!(is_business_license_active());
+        assert!(!is_enterprise_license_active());
+
+        // set Enterprise license
+        let users_limit = 15;
+        let devices_limit = 35;
+        let locations_limit = 5;
+        let network_devices_limit = 10;
+
+        let limits = LicenseLimits {
+            users: users_limit,
+            devices: devices_limit,
+            locations: locations_limit,
+            network_devices: Some(network_devices_limit),
+        };
+        let license = License::new(
+            "test".to_string(),
+            true,
+            Some(Utc::now() + TimeDelta::days(1)),
+            Some(limits),
+            None,
+            LicenseTier::Enterprise,
+        );
+        set_cached_license(Some(license));
+
+        assert!(is_business_license_active());
+        assert!(is_enterprise_license_active());
     }
 }
