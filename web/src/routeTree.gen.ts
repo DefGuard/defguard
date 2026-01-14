e72438a367d4e77b24b9d4c2c46e159bd50b2887
jakub-tldr <78603704+jakub-tldr@users.noreply.github.com>
2026-01-14 16:00:06 +0100

    update

--- web/src/routeTree.gen.ts
+++ web/src/routeTree.gen.ts
@@ -13,6 +13,7 @@ import { Route as SnackbarRouteImport } from './routes/snackbar'
 import { Route as PlaygroundRouteImport } from './routes/playground'
 import { Route as ConsentRouteImport } from './routes/consent'
 import { Route as AuthRouteImport } from './routes/auth'
+import { Route as AppLoaderRouteImport } from './routes/app-loader'
 import { Route as AuthorizedRouteImport } from './routes/_authorized'
 import { Route as R404RouteImport } from './routes/404'
 import { Route as AuthIndexRouteImport } from './routes/auth/index'
@@ -69,6 +70,11 @@ const ConsentRoute = ConsentRouteImport.update({
 const AuthRoute = AuthRouteImport.update({
   id: '/auth',
   path: '/auth',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AppLoaderRoute = AppLoaderRouteImport.update({
+  id: '/app-loader',
+  path: '/app-loader',
   getParentRoute: () => rootRouteImport,
 } as any)
 const AuthorizedRoute = AuthorizedRouteImport.update({
@@ -279,6 +285,7 @@ const AuthorizedDefaultLocationsLocationIdEditRoute =
 
 export interface FileRoutesByFullPath {
   '/404': typeof R404Route
+  '/app-loader': typeof AppLoaderRoute
   '/auth': typeof AuthRouteWithChildren
   '/consent': typeof ConsentRoute
   '/playground': typeof PlaygroundRoute
@@ -320,6 +327,7 @@ export interface FileRoutesByFullPath {
 }
 export interface FileRoutesByTo {
   '/404': typeof R404Route
+  '/app-loader': typeof AppLoaderRoute
   '/consent': typeof ConsentRoute
   '/playground': typeof PlaygroundRoute
   '/snackbar': typeof SnackbarRoute
@@ -362,6 +370,7 @@ export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/404': typeof R404Route
   '/_authorized': typeof AuthorizedRouteWithChildren
+  '/app-loader': typeof AppLoaderRoute
   '/auth': typeof AuthRouteWithChildren
   '/consent': typeof ConsentRoute
   '/playground': typeof PlaygroundRoute
@@ -406,6 +415,7 @@ export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
     | '/404'
+    | '/app-loader'
     | '/auth'
     | '/consent'
     | '/playground'
@@ -447,6 +457,7 @@ export interface FileRouteTypes {
   fileRoutesByTo: FileRoutesByTo
   to:
     | '/404'
+    | '/app-loader'
     | '/consent'
     | '/playground'
     | '/snackbar'
@@ -488,6 +499,7 @@ export interface FileRouteTypes {
     | '__root__'
     | '/404'
     | '/_authorized'
+    | '/app-loader'
     | '/auth'
     | '/consent'
     | '/playground'
@@ -532,6 +544,7 @@ export interface FileRouteTypes {
 export interface RootRouteChildren {
   R404Route: typeof R404Route
   AuthorizedRoute: typeof AuthorizedRouteWithChildren
+  AppLoaderRoute: typeof AppLoaderRoute
   AuthRoute: typeof AuthRouteWithChildren
   ConsentRoute: typeof ConsentRoute
   PlaygroundRoute: typeof PlaygroundRoute
@@ -566,6 +579,13 @@ declare module '@tanstack/react-router' {
       path: '/auth'
       fullPath: '/auth'
       preLoaderRoute: typeof AuthRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/app-loader': {
+      id: '/app-loader'
+      path: '/app-loader'
+      fullPath: '/app-loader'
+      preLoaderRoute: typeof AppLoaderRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/_authorized': {
@@ -946,6 +966,7 @@ const AuthRouteWithChildren = AuthRoute._addFileChildren(AuthRouteChildren)
 const rootRouteChildren: RootRouteChildren = {
   R404Route: R404Route,
   AuthorizedRoute: AuthorizedRouteWithChildren,
+  AppLoaderRoute: AppLoaderRoute,
   AuthRoute: AuthRouteWithChildren,
   ConsentRoute: ConsentRoute,
   PlaygroundRoute: PlaygroundRoute,
