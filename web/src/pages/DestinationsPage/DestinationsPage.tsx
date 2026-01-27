d5dc421c7a007ebd8384692aacd0402ebc10bbeb
Adam Ciarciński <adam@defguard.net>
2026-01-27 12:31:37 +0100

    Trim destinations

--- web/src/pages/DestinationsPage/DestinationsPage.tsx
+++ web/src/pages/DestinationsPage/DestinationsPage.tsx
@@ -1,37 +1,9 @@
-import { useMemo, useState } from 'react';
-import { AclDeploymentState, type AclDeploymentStateValue } from '../../shared/api/types';
 import { Page } from '../../shared/components/Page/Page';
-import { Tabs } from '../../shared/defguard-ui/components/Tabs/Tabs';
-import type { TabsItem } from '../../shared/defguard-ui/components/Tabs/types';
 
 export const DestinationsPage = () => {
-  const [activeTab, setActiveTab] = useState<AclDeploymentStateValue>(
-    AclDeploymentState.Applied,
-  );
-
-  const tabs = useMemo(
-    (): TabsItem[] => [
-      {
-        active: activeTab === AclDeploymentState.Applied,
-        onClick: () => {
-          setActiveTab(AclDeploymentState.Applied);
-        },
-        title: 'Deployed',
-      },
-      {
-        active: activeTab === AclDeploymentState.Modified,
-        onClick: () => {
-          setActiveTab(AclDeploymentState.Modified);
-        },
-        title: 'Pending',
-      },
-    ],
-    [activeTab],
-  );
 
   return (
     <Page id="aliases-page" title={'Destinations'}>
-      <Tabs items={tabs} />
     </Page>
   );
 };
