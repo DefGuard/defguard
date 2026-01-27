73c786cbc0e3223ac55aefb941f8027890375991
Adam Ciarciński <adam@defguard.net>
2026-01-27 13:52:18 +0100

    Default is false now

--- migrations/20260127094513_[2.0.0]_aclalias_any.up.sql
+++ migrations/20260127094513_[2.0.0]_aclalias_any.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE aclalias
-  ADD COLUMN any_destination boolean NOT NULL DEFAULT true,
-  ADD COLUMN any_port boolean NOT NULL DEFAULT true,
-  ADD COLUMN any_protocol boolean NOT NULL DEFAULT true;
+  ADD COLUMN any_destination boolean NOT NULL DEFAULT false,
+  ADD COLUMN any_port boolean NOT NULL DEFAULT false,
+  ADD COLUMN any_protocol boolean NOT NULL DEFAULT false;
