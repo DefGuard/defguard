ac809fd225fe44e1b012da70a010f56ae894b865
Adam Ciarciński <adam@defguard.net>
2026-01-27 12:47:33 +0100

    lint

--- web/src/pages/DestinationsPage/DestinationsPage.tsx
+++ web/src/pages/DestinationsPage/DestinationsPage.tsx
@@ -1,9 +1,5 @@
 import { Page } from '../../shared/components/Page/Page';
 
 export const DestinationsPage = () => {
-
-  return (
-    <Page id="aliases-page" title={'Destinations'}>
-    </Page>
-  );
+  return <Page id="aliases-page" title={'Destinations'}></Page>;
 };
