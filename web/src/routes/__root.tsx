11e4e0514812810d41309e871bd1d9398a3d70b0
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2026-02-04 16:36:26 +0100

    Fix wizard routing (#1991)
    * restore wizard routing
    
    * fix
    
    * redirect to login

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,26 +1,83 @@
 import type { QueryClient } from '@tanstack/react-query';
-import { createRootRouteWithContext, Outlet, redirect } from '@tanstack/react-router';
+import {
+  createRootRouteWithContext,
+  Outlet,
+  type ParsedLocation,
+  redirect,
+} from '@tanstack/react-router';
 import { AppLoaderPage } from '../pages/AppLoaderPage/AppLoaderPage';
+import { useSetupWizardStore } from '../pages/SetupPage/useSetupWizardStore';
 import { SnackbarManager } from '../shared/defguard-ui/providers/snackbar/SnackbarManager';
 import { useAuth } from '../shared/hooks/useAuth';
-import { getUserMeQueryOptions } from '../shared/query';
+import {
+  getSettingsEssentialsQueryOptions,
+  getUserMeQueryOptions,
+} from '../shared/query';
 
 interface RouterContext {
   queryClient: QueryClient;
 }
 
+// Handles the initial wizard redirect.
+// All routes should redirect to the setup wizard if the initial setup is not completed.
+const handleWizardRedirect = async ({
+  location,
+  context,
+}: {
+  location: ParsedLocation;
+  context: RouterContext;
+}) => {
+  const settingsEssentials = (
+    await (
+      await context.queryClient.ensureQueryData(getSettingsEssentialsQueryOptions)
+    )()
+  ).data;
+
+  // Tries to access any route but setup is not completed
+  const setupNotCompletedAnyAccess =
+    !settingsEssentials.initial_setup_completed &&
+    !location.pathname.startsWith('/setup-wizard');
+
+  // Tries to access setup wizard but setup is already completed
+  const setupCompletedButAccessingWizard =
+    settingsEssentials.initial_setup_completed &&
+    location.pathname.startsWith('/setup-wizard');
+
+  if (setupNotCompletedAnyAccess) {
+    useSetupWizardStore.getState().reset();
+    throw redirect({ to: '/setup-wizard', replace: true });
+  } else if (setupCompletedButAccessingWizard) {
+    throw redirect({ to: '/auth/login', replace: true });
+  }
+};
+
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootComponent,
   beforeLoad: async ({ location, context }) => {
-    if (location.pathname.startsWith('/auth')) {
+    await handleWizardRedirect({
+      location,
+      context,
+    });
+
+    if (
+      location.pathname.startsWith('/auth') ||
+      location.pathname.startsWith('/setup-wizard')
+    ) {
       return;
     }
+
     try {
       const user = (
         await (
           await context.queryClient.ensureQueryData(getUserMeQueryOptions)
         )()
       ).data;
+
+      // Invalid user object
+      if (!user.id) {
+        throw redirect({ to: '/auth/login', replace: true });
+      }
+
       useAuth.getState().setUser(user);
     } catch (_) {
       useAuth.getState().reset();
