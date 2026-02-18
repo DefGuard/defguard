9a17ec876ffb617e6cf8559ac87120f1f3e45c08
Jacek Chmielewski <jacek@defguard.net>
2026-02-18 06:58:42 +0100

    deleting a location cascade-deletes gateways

---
+++ migrations/20260218054705_[2.0.0]_gateway_cascade_delete.down.sql
@@ -0,0 +1,7 @@
+ALTER TABLE gateway
+DROP CONSTRAINT gateway_network_id_fkey;
+
+ALTER TABLE gateway
+ADD CONSTRAINT gateway_network_id_fkey
+FOREIGN KEY (network_id)
+REFERENCES wireguard_network(id);
