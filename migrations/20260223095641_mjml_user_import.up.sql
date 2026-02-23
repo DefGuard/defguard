ac9c46c8e814c2f9a8937e59964349198cd6a746
Filip Ślęzak <fslezak@teonite.com>
2026-02-23 11:49:16 +0100

    fix migration

--- migrations/20260223095641_mjml_user_import.up.sql
+++ migrations/20260223095641_mjml_user_import.up.sql
@@ -1,3 +1,3 @@
 INSERT INTO mail_context (template, section, language_tag, text) VALUES
     ('user-import-blocked', 'title', 'en_US', 'User import blocked'),
-    ('user-import-blocked', 'notification_text', 'Import of an external user was blocked because it would exceed your current license capacity.');
+    ('user-import-blocked', 'notification_text', 'en_US', 'Import of an external user was blocked because it would exceed your current license capacity.');
