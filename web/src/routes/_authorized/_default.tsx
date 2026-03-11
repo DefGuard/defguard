aaac205bf2f0340a7355e93cf7f1bbda6844d71c
Adam Ciarciński <adam@defguard.net>
2026-03-11 12:30:35 +0100

    Move to _authorized.tsx

--- web/src/routes/_authorized/_default.tsx
+++ web/src/routes/_authorized/_default.tsx
@@ -1,28 +1,7 @@
-import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
+import { createFileRoute, Outlet } from '@tanstack/react-router';
 import { Navigation } from '../../shared/components/Navigation/Navigation';
-import { getSessionInfoQueryOptions, getUserMeQueryOptions } from '../../shared/query';
 
 export const Route = createFileRoute('/_authorized/_default')({
-  beforeLoad: async ({ context, location }) => {
-    const sessionInfo = (await context.queryClient.fetchQuery(getSessionInfoQueryOptions))
-      .data;
-
-    if (sessionInfo.is_admin) {
-      return;
-    }
-
-    const me = (await context.queryClient.fetchQuery(getUserMeQueryOptions)).data;
-
-    if (location.pathname !== `/user/${me.username}`) {
-      throw redirect({
-        to: '/user/$username',
-        params: {
-          username: me.username,
-        },
-        replace: true,
-      });
-    }
-  },
   component: RouteComponent,
 });
 
