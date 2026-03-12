9868fc29d037a815128f832f0d15ac6c90984f3b
Filip Ślęzak <fslezak@teonite.com>
2026-03-12 10:51:37 +0100

    add confirm action modal

--- web/src/shared/hooks/modalControls/types.ts
+++ web/src/shared/hooks/modalControls/types.ts
@@ -1,3 +1,5 @@
+import type { QueryKey } from '@tanstack/react-query';
+import type { HTMLProps } from 'react';
 import type {
   AvailableLocationIpResponse,
   Device,
@@ -13,6 +15,19 @@ import type {
   User,
   Webhook,
 } from '../../api/types';
+import type { ButtonProps } from '../../defguard-ui/components/Button/types';
+
+export interface OpenConfirmActionModal {
+  title: string;
+  contentMd: string;
+  actionPromise: () => Promise<unknown>;
+  invalidateKeys?: QueryKey[];
+  cancelProps?: ButtonProps;
+  submitProps?: ButtonProps;
+  contentContainerProps?: HTMLProps<HTMLDivElement>;
+  onSuccess?: () => void;
+  onError?: () => void;
+}
 
 export interface OpenEditDeviceModal {
   device: Device;
