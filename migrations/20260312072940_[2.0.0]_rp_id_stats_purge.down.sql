a342c33177f38e68caa610e608015d6fb828e0bc
Jacek Chmielewski <jacek@defguard.net>
2026-03-13 13:23:32 +0100

    set purge setting default value

--- migrations/20260312072940_[2.0.0]_rp_id_stats_purge.down.sql
+++ migrations/20260312072940_[2.0.0]_rp_id_stats_purge.down.sql
@@ -5,4 +5,7 @@ UPDATE settings
     SET enable_stats_purge = NOT enable_stats_purge;
 
 ALTER TABLE settings
+    ALTER COLUMN enable_stats_purge SET DEFAULT false;
+
+ALTER TABLE settings
     RENAME COLUMN enable_stats_purge TO disable_stats_purge;
--- migrations/20260312072940_[2.0.0]_rp_id_stats_purge.up.sql
+++ migrations/20260312072940_[2.0.0]_rp_id_stats_purge.up.sql
@@ -2,6 +2,9 @@ ALTER TABLE settings
     RENAME COLUMN disable_stats_purge TO enable_stats_purge;
 
 ALTER TABLE settings
+    ALTER COLUMN enable_stats_purge SET DEFAULT true;
+
+ALTER TABLE settings
     DROP COLUMN webauthn_rp_id;
 
 UPDATE settings
