519288fc6a83ed7eaec10bd1b9063bea0ebfaeee
Maciej Wójcik <maciej@defguard.net>
2026-03-16 17:23:38 +0100

    add connected/disconnected events helpers

--- crates/defguard_session_manager/src/events.rs
+++ crates/defguard_session_manager/src/events.rs
@@ -2,14 +2,44 @@ use std::net::IpAddr;
 
 use chrono::NaiveDateTime;
 use defguard_common::db::{
-    Id,
     models::{Device, User, WireguardNetwork},
+    Id,
 };
 
 #[derive(Debug)]
 pub struct SessionManagerEvent {
     pub context: SessionManagerEventContext,
     pub event: SessionManagerEventType,
+}
+
+impl SessionManagerEvent {
+    #[must_use]
+    pub fn connected_for_session(
+        context: SessionManagerEventContext,
+        is_mfa_session: bool,
+    ) -> Self {
+        let event = if is_mfa_session {
+            SessionManagerEventType::MfaClientConnected
+        } else {
+            SessionManagerEventType::ClientConnected
+        };
+
+        Self { context, event }
+    }
+
+    #[must_use]
+    pub fn disconnected_for_session(
+        context: SessionManagerEventContext,
+        is_mfa_session: bool,
+    ) -> Self {
+        let event = if is_mfa_session {
+            SessionManagerEventType::MfaClientDisconnected
+        } else {
+            SessionManagerEventType::ClientDisconnected
+        };
+
+        Self { context, event }
+    }
 }
 
 #[derive(Debug)]
