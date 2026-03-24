4008655bcb3d2431eba9c2b6f2fceca44e3cf712
Jacek Chmielewski <jacek@defguard.net>
2026-03-24 07:53:41 +0100

    update backend & migrations to conform to new limited scope

--- migrations/20260323083929_[2.0.0]_enrollment_settings.down.sql
+++ migrations/20260323083929_[2.0.0]_enrollment_settings.down.sql
@@ -1,12 +1,2 @@
 ALTER TABLE settings
-    DROP COLUMN enrollment_macos_release_channel,
-    DROP COLUMN enrollment_linux_release_channel,
-    DROP COLUMN enrollment_windows_release_channel,
-    DROP COLUMN enrollment_send_welcome_email,
-    DROP COLUMN enrollment_show_welcome_message,
-    DROP COLUMN enrollment_show_reset_password,
-    DROP COLUMN enrollment_admin_custom_email,
-    DROP COLUMN enrollment_admin_email_mode;
-
-DROP TYPE enrollment_release_channel;
-DROP TYPE enrollment_admin_email_mode;
+    DROP COLUMN enrollment_send_welcome_email;
