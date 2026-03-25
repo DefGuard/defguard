0c51dc2ae6fd08a81bb5963f4be782dea28b2d0d
Adam Ciarciński <adam@defguard.net>
2026-03-25 15:46:16 +0100

    ACL rule generator

---
+++ tools/defguard_generator/src/acl_rules.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use defguard_core::enterprise::db::models::acl::{AclRule, RuleState};
+use sqlx::{PgPool, query};
+
+pub async fn generate_acl_rules(pool: PgPool, num_rules: u32) -> Result<()> {
+    truncate_with_restart(&pool).await?;
+
+    for index in 1..num_rules {
+        let mut acl_rule = AclRule::default();
+        acl_rule.name = format!("Generated {index}");
+        acl_rule.state = RuleState::Applied;
+        acl_rule.all_locations = true;
+        acl_rule.allow_all_users = true;
+        acl_rule.allow_all_groups = true;
+        acl_rule.allow_all_network_devices = true;
+        // acl_rule.any_port = false;
+        // acl_rule.ports = vec![(index as i32..=index as i32).into()];
+        acl_rule.save(&pool).await?;
+    }
+
+    Ok(())
+}
+
+/// Remove all records from sessions and stats tables.
+/// This also resets the auto-incrementing sequences.
+async fn truncate_with_restart(pool: &PgPool) -> Result<()> {
+    query("TRUNCATE aclrule RESTART IDENTITY CASCADE")
+        .execute(pool)
+        .await?;
+
+    Ok(())
+}
