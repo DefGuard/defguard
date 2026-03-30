75e71aee01813563b7501f5a4502ef1ccfb9f70b
Jacek Chmielewski <jacek@defguard.net>
2026-03-30 12:57:14 +0200

    download CA cert button

--- web/src/pages/settings/SettingsCaPage/SettingsCaPage.tsx
+++ web/src/pages/settings/SettingsCaPage/SettingsCaPage.tsx
@@ -13,6 +13,7 @@ import caIconSrc from '../../SetupPage/assets/ca.png';
 import { SizedBox } from '../../../shared/defguard-ui/components/SizedBox/SizedBox';
 import { ThemeSpacing } from '../../../shared/defguard-ui/types';
 import { Divider } from '../../../shared/defguard-ui/components/Divider/Divider';
+import { Button } from '../../../shared/defguard-ui/components/Button/Button';
 
 const breadcrumbs = [
   <Link
@@ -63,12 +64,14 @@ const Content = () => {
       <p className='ca-info-title'>{m.settings_certs_ca_information_extracted()}</p>
       <Divider spacing={ThemeSpacing.Md}/>
       <div className='ca-info-grid'>
-        <div className='ca-info-label'>Email</div>
-        <div className='ca-info-value'>test@mail.com</div>
-        <div className='ca-info-label'>Valid by</div>
-        <div className='ca-info-value'>25/03/2057  | 16:23:00</div>
+        <div className='ca-info-label'>{m.settings_certs_ca_email()}</div>
+        <div className='ca-info-value'>TODO</div>
+        <div className='ca-info-label'>{m.settings_certs_ca_valid_until()}</div>
+        <div className='ca-info-value'>TODO</div>
       </div>
       <Divider spacing={ThemeSpacing.Md}/>
+      <SizedBox height={ThemeSpacing.Xl2} />
+      <Button variant='outlined' text={m.settings_certs_ca_download()} iconLeft='download'/>
     </ActionableSection>
   );
 };
