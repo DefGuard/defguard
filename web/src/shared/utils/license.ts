b587d308ae3613799122ba558d9c5e880d206c53
Jacek Chmielewski <jacek@defguard.net>
2026-03-30 07:35:32 +0200

    support type labels

--- web/src/shared/utils/license.ts
+++ web/src/shared/utils/license.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
-import type { LicenseInfo } from '../api/types';
+import { m } from '../../paraglide/messages';
+import { type LicenseInfo, SupportType, type SupportTypeValue } from '../api/types';
 import { openModal } from '../hooks/modalControls/modalsSubjects';
 import { ModalName } from '../hooks/modalControls/modalTypes';
 
@@ -44,6 +45,25 @@ export const getLicenseState = (
   }
 
   return 'validBusiness';
+};
+
+export const getSupportTypeLabel = (supportType: SupportTypeValue): string => {
+  switch (supportType) {
+    case SupportType.Unspecified:
+      return m.license_support_type_unspecified();
+    case SupportType.Free:
+      return m.license_support_type_free();
+    case SupportType.Basic:
+      return m.license_support_type_basic();
+    case SupportType.Direct:
+      return m.license_support_type_direct();
+    case SupportType.BasicEnterprise:
+      return m.license_support_type_basic_enterprise();
+    case SupportType.DirectEnterprise:
+      return m.license_support_type_direct_enterprise();
+    default:
+      return supportType;
+  }
 };
 
 export const licenseActionCheck = (
