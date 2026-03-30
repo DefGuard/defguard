4829254768a5846ba782b532acc82e3b8f526f9a
Adam Ciarciński <adam@defguard.net>
2026-03-30 14:02:12 +0200

    Fix user profile view

--- web/src/shared/query.ts
+++ web/src/shared/query.ts
@@ -93,7 +93,7 @@ export const userProfileQueryOptions = (username: string) =>
     queryFn: () => api.user.getUser(username),
     select: ({ data }) => {
       const res: UserProfile = {
-        devices: data.devices.map((device) => ({
+        devices: data.user.devices.map((device) => ({
           ...device,
           biometry_enabled: data.biometric_enabled_devices.includes(device.id),
         })),
