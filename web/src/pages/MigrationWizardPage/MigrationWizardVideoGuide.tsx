9e52b4f85efe98e79907245848c42fbf36f26d3b
Jacek Chmielewski <jacek@defguard.net>
2026-04-10 09:21:54 +0200

    add helper, tweak styling

--- web/src/pages/MigrationWizardPage/MigrationWizardVideoGuide.tsx
+++ web/src/pages/MigrationWizardPage/MigrationWizardVideoGuide.tsx
@@ -3,6 +3,8 @@ import { m } from '../../paraglide/messages';
 import { AppText } from '../../shared/defguard-ui/components/AppText/AppText';
 import { Divider } from '../../shared/defguard-ui/components/Divider/Divider';
 import { ExternalLink } from '../../shared/defguard-ui/components/ExternalLink/ExternalLink';
+import { Helper } from '../../shared/defguard-ui/components/Helper/Helper';
+import { Icon } from '../../shared/defguard-ui/components/Icon/Icon';
 import { SizedBox } from '../../shared/defguard-ui/components/SizedBox/SizedBox';
 import { TextStyle, ThemeSpacing, ThemeVariable } from '../../shared/defguard-ui/types';
 import { Thumbnail } from '../../shared/video-tutorials/components/widget/Thumbnail/Thumbnail';
@@ -23,9 +25,12 @@ export const MigrationWizardVideoGuide = () => {
         <SizedBox height={ThemeSpacing.Xl5} />
 
         <div>
-          <AppText font={TextStyle.TBodySm500} color={ThemeVariable.FgFaded}>
-            {m.migration_wizard_support_video_guide()}
-          </AppText>
+          <div className="migration-wizard-support-header">
+            <Helper size={16}>{m.migration_wizard_support_video_guide_helper()}</Helper>
+            <AppText font={TextStyle.TBodySm500} color={ThemeVariable.FgFaded}>
+              {m.migration_wizard_support_video_guide()}
+            </AppText>
+          </div>
           <SizedBox height={ThemeSpacing.Md} />
           <button
             type="button"
@@ -49,9 +54,12 @@ export const MigrationWizardVideoGuide = () => {
         </div>
         <Divider spacing={ThemeSpacing.Xl2} />
         <div>
-          <AppText font={TextStyle.TBodySm500} color={ThemeVariable.FgFaded}>
-            {m.migration_wizard_support_related_documentation()}
-          </AppText>
+          <div className="migration-wizard-support-header">
+            <Icon icon="file" size={16} staticColor={ThemeVariable.FgMuted} />
+            <AppText font={TextStyle.TBodySm500} color={ThemeVariable.FgFaded}>
+              {m.migration_wizard_support_related_documentation()}
+            </AppText>
+          </div>
           <SizedBox height={ThemeSpacing.Md} />
           <div className="migration-wizard-doc-card">
             <ExternalLink href={videoGuide.docsUrl} target="_blank" rel="noreferrer">
