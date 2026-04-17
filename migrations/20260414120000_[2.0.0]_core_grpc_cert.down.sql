0641a8978e2a6c879574db4ad288e57ad9961ad9
Maciej Wójcik <maciej@defguard.net>
2026-04-17 12:26:32 +0200

    adjust down migration

--- migrations/20260414120000_[2.0.0]_core_grpc_cert.down.sql
+++ migrations/20260414120000_[2.0.0]_core_grpc_cert.down.sql
@@ -2,11 +2,11 @@ ALTER TABLE gateway RENAME COLUMN certificate_serial TO certificate;
 ALTER TABLE proxy   RENAME COLUMN certificate_serial TO certificate;
 
 ALTER TABLE gateway
-    DROP COLUMN IF EXISTS core_client_cert_der,
-    DROP COLUMN IF EXISTS core_client_cert_key_der,
-    DROP COLUMN IF EXISTS core_client_cert_expiry;
+    DROP COLUMN core_client_cert_der,
+    DROP COLUMN core_client_cert_key_der,
+    DROP COLUMN core_client_cert_expiry;
 
 ALTER TABLE proxy
-    DROP COLUMN IF EXISTS core_client_cert_der,
-    DROP COLUMN IF EXISTS core_client_cert_key_der,
-    DROP COLUMN IF EXISTS core_client_cert_expiry;
+    DROP COLUMN core_client_cert_der,
+    DROP COLUMN core_client_cert_key_der,
+    DROP COLUMN core_client_cert_expiry;
