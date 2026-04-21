093bc8a95a429330e7c394176e0310587c07e7d1
Maciej Wójcik <maciej@defguard.net>
2026-04-21 13:43:53 +0200

    remove unused module

--- web/src/shared/video-tutorials/version.ts
+++
@@ -1,5 +0,0 @@
-// Re-export version utilities from the shared utils module so that existing
-// consumers of this path continue to work without changes.
-
-export type { ParsedVersion } from '../utils/resolveVersion';
-export { compareVersions, parseVersion } from '../utils/resolveVersion';
