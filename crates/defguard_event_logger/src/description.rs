a657f23bdb5a7df0df25c38cf99a33c75777daac
Kuba <78603704+jakub-tldr@users.noreply.github.com>
2026-05-04 12:17:05 +0200

    change message

--- crates/defguard_event_logger/src/description.rs
+++ crates/defguard_event_logger/src/description.rs
@@ -26,10 +26,9 @@ pub fn get_defguard_event_description(event: &DefguardEvent) -> Option<String> {
         } => Some(format!(
             "User login using {mfa_method} failed with: {message}"
         )),
-        DefguardEvent::RecoveryCodeLoginFailed => Some(
-            "User login using recovery code failed with: Recovery code verification failed"
-                .to_string(),
-        ),
+        DefguardEvent::RecoveryCodeLoginFailed => {
+            Some("User login with a recovery code failed".to_string())
+        }
         DefguardEvent::UserLogout => None,
         DefguardEvent::RecoveryCodeUsed => None,
         DefguardEvent::PasswordChanged => None,
