1eed03699b6c91d2bb3111707e81b5c0c63ff96f
Kuba <78603704+jakub-tldr@users.noreply.github.com>
2026-06-10 13:04:00 +0200

    clippy lint

--- crates/defguard_core/src/enterprise/handlers/openid_login.rs
+++ crates/defguard_core/src/enterprise/handlers/openid_login.rs
@@ -343,14 +343,14 @@ pub async fn user_from_claims(
 
                 // If user synchronization is enabled and limited to specific directory groups, only allow
                 // creating accounts for users who are members of one of those groups.
-                if provider.directory_sync_enabled {
-                    if let Some(user_groups_filter) = provider
+                if provider.directory_sync_enabled
+                    && let Some(user_groups_filter) = provider
                         .directory_sync_user_groups
                         .as_ref()
                         .filter(|groups| !groups.is_empty())
-                    {
-                        if provider.kind == OpenIdProviderKind::Microsoft {
-                            let in_groups = user_in_directory_groups(pool, email, user_groups_filter)
+                    && provider.kind == OpenIdProviderKind::Microsoft
+                {
+                    let in_groups = user_in_directory_groups(pool, email, user_groups_filter)
                             .await
                             .map_err(|err| {
                                 error!(
@@ -361,19 +361,18 @@ pub async fn user_from_claims(
                                     "Failed to verify user's directory group membership".into(),
                                 )
                             })?;
-                            if !in_groups {
-                                warn!(
+                    if !in_groups {
+                        warn!(
                                 "User with email address {} is trying to log in for the first time but is not a member of any
                                 directory groups configured for user synchronization. Blocking account creation.",
                                 email.as_str()
                             );
-                                return Err(WebError::UserGroupsNotSynced(
+                        return Err(WebError::UserGroupsNotSynced(
                                 "User is not a member of any of the directory groups allowed for account creation".into(),
                             ));
-                            }
-                        }
                     }
                 }
+
                 // Try to get the username from `preferred_username` claim.
                 // If it's not there, extract it from email.
                 let username = if let Some(username) = token_claims.preferred_username() {
