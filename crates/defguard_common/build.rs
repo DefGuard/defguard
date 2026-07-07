791e72a46aa4064d44d5d4d3d915c31b082f7a20
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2026-07-07 10:14:27 +0200

    fix

--- crates/defguard_common/build.rs
+++ crates/defguard_common/build.rs
@@ -1,8 +1,8 @@
-use vergen_git2::{Emitter, Git2Builder};
+use vergen_git2::{Emitter, Git2};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // set VERGEN_GIT_SHA env variable based on git commit hash
-    let git2 = Git2Builder::default().sha(true).build()?;
+    let git2 = Git2::builder().sha(true).build();
     Emitter::default().add_instructions(&git2)?.emit()?;
 
     println!("cargo:rerun-if-changed=../../migrations");
