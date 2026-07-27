7e2069214ddbce6faab1afc316b0c14a0e353dea
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2026-07-27 11:33:23 +0200

    Properly display Entra login error (#3464)
    * show error on login
    
    * linter
    
    * change method of dispalying the error
    
    * add comment

--- web/src/routes/auth/login.tsx
+++ web/src/routes/auth/login.tsx
@@ -1,6 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
+import z from 'zod';
 import { LoginMainPage } from '../../pages/auth/LoginMain/LoginMainPage';
 
+const searchSchema = z.object({
+  authError: z.string().optional(),
+});
+
 export const Route = createFileRoute('/auth/login')({
+  validateSearch: searchSchema,
   component: LoginMainPage,
 });
