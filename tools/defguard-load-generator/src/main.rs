be773d5927f421b116da4c6729ecc207153f5854
Jacek Chmielewski <jacek@defguard.net>
2026-09-09 13:31:30 +0200

    basic config

--- tools/defguard-load-generator/src/main.rs
+++ tools/defguard-load-generator/src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use tracing_subscriber::EnvFilter;
+use clap::Parser;
+
+mod config;
+
+fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .init();
+
+    let config = config::Config::parse();
+    tracing::info!(?config, "running with config");
+}
